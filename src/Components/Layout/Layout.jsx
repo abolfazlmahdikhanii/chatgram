52c1fbf570751f8dab5a010191ed7345c4524450
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-08-24 22:32:59 +0330

    modified

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import SideMenu from '../SideMenu/SideMenu'
+import MessageList from '../MessageList/MessageList'
 
 const Layout = () => {
   return (
-    <div>
-        
+    <div className='grid-cols-[95px_300px,1fr_250px] h-screen grid gap-x-4'>
+        <SideMenu/>
+        <MessageList/>
     </div>
   )
 }
