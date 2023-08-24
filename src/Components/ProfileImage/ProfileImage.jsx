edaaeb5503ffb19d1b64933ac11ae006981d1113
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-08-24 22:33:29 +0330

    create component for profile

---
+++ src/Components/ProfileImage/ProfileImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileImage = () => {
+  return (
+    <div className='w-12 h-12 rounded-xl overflow-hidden bg-indigo-500'>
+        <img src="images/profile.jpg" alt="" className='w-full h-full'/>
+    </div>
+  )
+}
+
+export default ProfileImage
