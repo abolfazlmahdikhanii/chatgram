7c57695d800c87f3baa222002f83115ebd096056
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-08-25 22:07:51 +0330

    modified

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import SideMenu from '../SideMenu/SideMenu'
 import MessageList from '../MessageList/MessageList'
+import Chat from '../../Pages/Chat/Chat'
+import ChatInfo from '../ChatInfo/ChatInfo'
 
 const Layout = () => {
   return (
-    <div className='grid-cols-[95px_300px,1fr_250px] h-screen grid gap-x-4'>
+    <div className='grid-cols-[95px_350px,1fr_250px] h-screen grid  overflow-hidden'>
         <SideMenu/>
         <MessageList/>
+        <Chat/>
+        <ChatInfo/>
     </div>
   )
 }
