f6ba4a5f340c0adcdbc8cb7f9428b1d980f5eee6
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-08-28 15:54:18 +0330

    display message

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -3,32 +3,48 @@ import { BiCheckDouble } from "react-icons/bi";
 import { BiCheck } from "react-icons/bi";
 import { Watch } from "react-loader-spinner";
 import ProfileImage from "../ProfileImage/ProfileImage";
-const Message = ({ from, txt,userInfo }) => {
+const Message = ({ from, messageDis, date,read,send, userInfo }) => {
+  const formatTime = (date) => {
+    return new Intl.DateTimeFormat("tr", {
+      hour: "2-digit",
+      minute: "2-digit",
+    }).format(date);
+  };
+
+  let icon = null;
+  if (read) {
+    icon = <BiCheckDouble size={14} color="" />;
+  } else if (send) {
+    icon = <BiCheck size={14} color="" />;
+  } else {
+    icon = (
+      <Watch
+        height="10"
+        width="10"
+        radius="48"
+        color="#ffff"
+        ariaLabel="watch-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    );
+  }
+
   return (
+    <div
+      className={`message ${
+        from === "user" ? "message--user" : "message--client"
+      }`}
+    >
+      <div className="pt-2 pb-1 px-1.5">{messageDis}</div>
+      <div className=" flex items-center gap-1.5 mb-0.5 px-1">
+        <p className="text-gray-400 text-[10px]">{formatTime(date)}</p>
 
-      <div
-        className={`message ${
-          from === "user" ? "message--user" : "message--client"
-        }`}
-      >
-        <div>{txt}</div>
-        <div className="mt-3 flex items-center gap-1.5 mb-0.5 px-1">
-          <p className="text-gray-400 text-[11px]">12:24</p>
-
-          {/* <Watch
-          height="10"
-          width="10"
-          radius="48"
-          color="#ffff"
-          ariaLabel="watch-loading"
-          wrapperStyle={{}}
-          wrapperClassName=""
-          visible={true}
-        /> */}
-          <BiCheckDouble size={16} color="" />
-        </div>
+    
+        {icon}
       </div>
-
+    </div>
   );
 };
 
