bfeccbf09df1987b1e31ae09606149fac74d30cf
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-08-28 15:54:42 +0330

    display all messages

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatHeader from "../../Components/ChatHeader/ChatHeader";
 import Message from "../../Components/Message/Message";
 import ChatForm from "../../Components/ChatForm/ChatForm";
+import { useParams } from "react-router-dom";
 
-const Chat = () => {
+const Chat = ({ chat }) => {
+  const [message, setMessage] = useState();
+
+  const match = useParams();
+
+  useEffect(() => {
+    filterChat(match.id);
+    console.log(message)
+  }, [match.id]);
+
+  const filterChat = (id) => {
+    let findChat = chat.find((item) => item.id == id);
+
+  
+    setMessage(findChat);
+  };
+
   return (
     <div className="bg-[url('../../../src/assets/images/bg-pattern.svg')] h-screen ">
-      <ChatHeader />
+      <ChatHeader info={message}/>
       <main className="flex flex-col justify-between h-full  overflow-hidden">
         <section className="h-[90%] overflow-y-auto px-5 flex flex-col gap-5 mt-1 mb-1.5">
-            <Message from="user"/>
-            <Message from="client"/>
-            <Message from="client"/>
+          {message?.messages?.map((item) => (
+            <Message key={item.messageId} from={item.from} {...item} />
+          ))}
         </section>
 
         {/* FORM */}
-        <ChatForm/>
+        <ChatForm />
       </main>
     </div>
   );
