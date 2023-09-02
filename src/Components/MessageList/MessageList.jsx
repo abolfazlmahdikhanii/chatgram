fe79abf2feb240bae26fb992a0a5c7add008e3bf
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-02 17:30:11 +0330

    display message with emoji and text

--- src/Components/MessageList/MessageList.jsx
+++ src/Components/MessageList/MessageList.jsx
@@ -3,6 +3,10 @@ import Box from "../UI/Box/Box";
 import MessageItem from "../Messageitem/MessageItem";
 
 const MessageList = ({chats}) => {
+<<<<<<< HEAD
+=======
+    
+>>>>>>> 3e4f26d (display message with emoji and text)
 
   return (
     <Box style={"ml-3"}>
