f487a1a2989feeabd6dc638776e5e598942246bf
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-02 17:28:27 +0330

    modified

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -4,15 +4,24 @@ import Message from "../../Components/Message/Message";
 import ChatForm from "../../Components/ChatForm/ChatForm";
 import { useParams } from "react-router-dom";
 
+<<<<<<< HEAD
 const Chat = ({ chat }) => {
+=======
+const Chat = ({ chat,setChat }) => {
+>>>>>>> 5b8b344 (modified)
   const [message, setMessage] = useState();
 
   const match = useParams();
 
   useEffect(() => {
     filterChat(match.id);
+<<<<<<< HEAD
     console.log(message)
   }, [match.id]);
+=======
+   
+  }, [match,message]);
+>>>>>>> 5b8b344 (modified)
 
   const filterChat = (id) => {
     let findChat = chat.find((item) => item.id == id);
@@ -21,18 +30,49 @@ const Chat = ({ chat }) => {
     setMessage(findChat);
   };
 
+<<<<<<< HEAD
   return (
     <div className="bg-[url('../../../src/assets/images/bg-pattern.svg')] h-screen ">
       <ChatHeader info={message}/>
       <main className="flex flex-col justify-between h-full  overflow-hidden">
         <section className="h-[90%] overflow-y-auto px-5 flex flex-col gap-5 mt-1 mb-1.5">
+=======
+  const sendMessageHandler = (txt) => {
+
+    const message = {
+      messageId: crypto.randomUUID(),
+      messageDis: txt,
+      from: "user",
+      to: "ab",
+      date: new Date(),
+      read: false,
+      send: true,
+    };
+    const newChat=[...chat]
+
+    const findedChat = newChat.find((item) => item.id == match?.id);
+    findedChat.messages.push(message);
+
+    setChat(newChat);
+  };
+
+  return (
+    <div className="bg-[url('../../../src/assets/images/bg-pattern.svg')] h-screen relative">
+      <ChatHeader info={message}/>
+      <main className="flex flex-col justify-between h-full  overflow-hidden">
+        <section className="h-[90%] overflow-y-auto px-5 flex flex-col gap-5 mt-1 mb-1.5 transition-all duration-200">
+>>>>>>> 5b8b344 (modified)
           {message?.messages?.map((item) => (
             <Message key={item.messageId} from={item.from} {...item} />
           ))}
         </section>
 
         {/* FORM */}
+<<<<<<< HEAD
         <ChatForm />
+=======
+        <ChatForm set={sendMessageHandler} />
+>>>>>>> 5b8b344 (modified)
       </main>
     </div>
   );
