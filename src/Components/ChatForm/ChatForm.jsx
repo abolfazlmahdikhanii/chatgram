fb5ec99dd8539a6f46ec8af41819ab00e60db0aa
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-02 17:28:48 +0330

    add emoji

--- src/Components/ChatForm/ChatForm.jsx
+++ src/Components/ChatForm/ChatForm.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import BtnAction from "../UI/BtnAction/BtnAction";
 import BtnUploader from "../BtnUploader/BtnUploader";
@@ -5,11 +6,63 @@ import BtnUploader from "../BtnUploader/BtnUploader";
 const ChatForm = () => {
   return (
     <form action="#" className=" w-11/12 mx-auto  mb-24 flex gap-3.5 h-[48px]">
+=======
+import React, { useRef, useEffect, useState } from "react";
+
+import Picker from "@emoji-mart/react";
+
+import BtnAction from "../UI/BtnAction/BtnAction";
+import BtnUploader from "../BtnUploader/BtnUploader";
+import ContentWrapper from "../ContentWrapper/ContentWrapper";
+
+const ChatForm = ({ set }) => {
+  const [text, setText] = useState("");
+  const [emoji, setEmoji] = useState([]);
+  const [showEmoji, setShowEmoji] = useState(false);
+
+  const inputRef = useRef(null);
+
+  useEffect(()=>{
+    setText(inputRef.current);
+
+  },[inputRef])
+
+  // find chat with id and store  in the object in the place
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    if (inputRef.current.innerHTML!=="") {
+      
+      set(text.innerHTML);
+      inputRef.current.innerHTML=""
+    }
+  };
+  const closeEmojiPicker = () => {
+    setTimeout(() => {
+      setShowEmoji(false);
+    }, 500);
+  };
+
+  return (
+    <form
+      action="#"
+      className=" w-11/12 mx-auto  mb-24 flex gap-3.5 h-[48px] max-w-2xl"
+      onSubmit={submitFormHandler}
+    >
+>>>>>>> 63e42ce (add emoji)
       {/* input */}
       <div className="bg-gray-900 flex items-center justify-between py-1.5 rounded-xl w-full px-3">
         <div className="flex items-center gap-0.5 w-full">
           {/* emoji */}
+<<<<<<< HEAD
           <button className="text-yellow-500 grid place-items-center">
+=======
+          <button
+            className="text-yellow-500 grid place-items-center"
+            onMouseEnter={() => {
+              setShowEmoji((prev) => !prev);
+            }}
+          >
+>>>>>>> 63e42ce (add emoji)
             <svg
               width={24}
               height={24}
@@ -35,6 +88,7 @@ const ChatForm = () => {
             </svg>
           </button>
 
+<<<<<<< HEAD
           <input
             type="text"
             placeholder="Write message to User..."
@@ -48,6 +102,56 @@ const ChatForm = () => {
       </div>
    
    <BtnAction/>
+=======
+          <div
+            type="text"
+            placeholder="Write message to User..."
+            className="chat__input overflow-hidden"
+            // onInput={(e) => setText(e.target.innerHTML)}
+            ref={inputRef}
+            contentEditable
+            suppressContentEditableWarning={true}
+          >
+            {emoji.map((item, i) => (
+              <img
+                key={i}
+                src={`https://cdn.jsdelivr.net/npm/emoji-datasource-apple@14.0.0/img/apple/64/${item.unified}.png`}
+                className="w-[20px] h-[20px] inline-flex"
+              />
+            ))}
+          </div>
+        </div>
+        {/* action */}
+        <div>
+          <BtnUploader />
+        </div>
+      </div>
+
+      <BtnAction />
+      {/* emoji */}
+      <div className="absolute bottom-20 left-14" onMouseOut={closeEmojiPicker}>
+        {showEmoji && (
+          <Picker
+            onEmojiSelect={(e) =>
+              setEmoji((prev) => {
+                return [
+                  ...prev,
+                  {
+                    id: e.id,
+                    shortcodes: e.shortcodes,
+                    unified: e.unified,
+                  },
+                ];
+              })
+            }
+            emojiSize={25}
+            previewPosition="none"
+            value={text}
+            set={"apple"}
+          />
+        )}
+      </div>
+>>>>>>> 63e42ce (add emoji)
     </form>
   );
 };
