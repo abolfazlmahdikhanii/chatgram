8b84cb6480b12a94d52656c7e5fecb94dbd6c4af
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-03 20:23:09 +0330

    create component for upload

---
+++ src/Components/Uploader/Uploader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ModalPerview from '../UI/ModalPerview/ModalPerview'
+
+const Uploader = () => {
+  return (
+    <div>
+
+        <ModalPerview/>
+    </div>
+  )
+}
+
+export default Uploader
