df9be8e4a08680dd0a5b9cefb0d6ee4bfaefee98
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-04 22:05:55 +0330

    change rouunded image

--- src/Components/ProfileImage/ProfileImage.jsx
+++ src/Components/ProfileImage/ProfileImage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const ProfileImage = ({size,src}) => {
   return (
-    <div className={`profile-img ${size==="m"?'w-14 h-14':'w-11 h-11'}`}>
+    <div className='avatar'>
+      <div className={`mask mask-squircle ${size==="m"?'w-14 h-14':'w-11 h-11'}`}>
         <img src={src?src:'../../../src/assets/images/profile.jpg'} alt="profile" className='w-full h-full'/>
+    </div>
     </div>
   )
 }
