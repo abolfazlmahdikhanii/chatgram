59e1e172b239b06b32c0281e2a1a4a79dc552cd0
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-04 22:06:53 +0330

    modified

--- src/Components/Uploader/Uploader.jsx
+++ src/Components/Uploader/Uploader.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ModalPerview from '../UI/ModalPerview/ModalPerview'
+import ModalPerviewFile from '../UI/ModalPerviewFile/ModalPerviewFile'
 
 const Uploader = () => {
   return (
     <div>
 
-        <ModalPerview/>
+        {/* <ModalPerview/> */}
+        <ModalPerviewFile/>
     </div>
   )
 }
