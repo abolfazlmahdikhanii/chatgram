7d84e464e861f86de44a7916e2abd4782a90062a
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-06 13:47:33 +0330

    modified

--- src/Components/UI/BtnAction/BtnAction.jsx
+++ src/Components/UI/BtnAction/BtnAction.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BtnAction = () => {
   return (
-    <button className="h-full px-[16px] btn btn-primary rounded-xl">
+    <button className="h-full px-[16px] btn btn-primary rounded-xl" >
     <svg
       width={19}
       height={19}
