ea7e5e49225e391062c0d711d39ce2249fc040ad
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-09-15 21:58:24 +0330

    modified for audio file

--- src/Components/FIleItem/FileItem.jsx
+++ src/Components/FIleItem/FileItem.jsx
@@ -20,39 +20,91 @@ const FileItem = ({
     return parseFloat((bytes / Math.pow(k, i)).toFixed()) + " " + sizes[i];
   };
   return (
-    <li
-      className={`file-item relative  w-full ${
-        message ? "hover:bg-transparent" : "px-3"
-      }`}
-    >
-      <div>
-        <FileIcon
-          type={type}
-          path={src ? src : ""}
-          message={message}
-          from={from}
+    <>
+     {
+      !message?(
+        <DefaultFile
+         type={type}
+         src={src}
+         message={message}
+         from={from}
+         size={size}
+         name={name}
+         onRemove={onRemove}
+         formatSize={formatSize}
         />
-      </div>
-      <div className="flex flex-col gap-2 max-w-[200px]">
-        <p
-          className={`${
-            !message ? "text-lg" : "text-sm"
-          } text-white font-semibold truncate w-full `}
-        >
-          {name}
-        </p>
-        <p className={`${message ? "text-xs" : ""}`}>{formatSize(size)}</p>
-        {message && <ProgressFile />}
-      </div>
-      {!message && (
-        <div className="self-center  w-full flex justify-end">
-          <button className="btn btn-square btn-sm " onClick={onRemove}>
-            <MdOutlineDeleteOutline size={21} />
-          </button>
-        </div>
-      )}
-    </li>
+      ):
+      (
+        <MessageFile
+         type={type}
+         src={src}
+         message={message}
+         from={from}
+         size={size}
+         name={name}
+         formatSize={formatSize}
+        />
+      )
+     }
+    </>
   );
 };
-
+const DefaultFile=({type,src,message,from,size,name,onRemove,formatSize})=>{
+  return(
+    <li
+    className={`file-item relative  w-full px-3`}
+  >
+    <div>
+      <FileIcon
+        type={type}
+        path={src ? src : ""}
+        message={message}
+        from={from}
+      />
+    </div>
+    <div className="flex flex-col gap-2 max-w-[200px]">
+      <p
+        className={`text-lg text-white font-semibold truncate w-full `}
+      >
+        {name}
+      </p>
+      <p >{formatSize(size)}</p>
+     
+    </div>
+  
+      <div className="self-center  w-full flex justify-end">
+        <button className="btn btn-square btn-sm " onClick={onRemove}>
+          <MdOutlineDeleteOutline size={21} />
+        </button>
+      </div>
+    
+  </li>
+  )
+}
+const MessageFile=({type,src,message,from,size,name,formatSize})=>{
+  return(
+  <li
+  className={`file-item relative  w-full hover:bg-transparent`}
+>
+  <div>
+    <FileIcon
+      type={type}
+      path={src ? src : ""}
+      message={message}
+      from={from}
+    />
+  </div>
+  <div className="flex flex-col gap-2 max-w-[200px]">
+    <p
+      className={`text-sm text-white font-semibold truncate w-full `}
+    >
+      {name}
+    </p>
+    <p className={`text-xs`}>{formatSize(size)}</p>
+    <ProgressFile />
+  </div>
+ 
+</li>
+  )
+}
 export default FileItem;
