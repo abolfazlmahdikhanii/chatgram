68eab4fde399e83f6121a5fec634bbeb93a46172
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-10-27 19:36:41 +0330

    modified

--- src/Components/PinBox/PinMessage.jsx
+++ src/Components/PinBox/PinMessage.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
-const PinMessage = () => {
+const PinMessage = ({title,index}) => {
+
   return (
-    <div className='flex gap-3'>
-                <div className='w-11 h-full mask mask-squircle'>
-                    <img src="../../../src/assets/images/profile.jpg" alt="" className='w-full h-full object-cover' />
-                </div>
-                <div className='flex flex-col gap-0.5'>
-                    <p className='font-semibold text-indigo-500 text-sm'>Pinned message #1</p>
-                    <p className='text-[13px] '>الگوی بازگشتیcaps</p>
-                </div>
-            </div>
+    <>
+  {
+    index===0&&  <div className='flex gap-3'>
+   {
+    typeof title!=="string"||title?.type==="img"&& 
+    <div className='w-11 h-full mask mask-squircle'>
+    <img src="../../../src/assets/images/profile.jpg" alt="" className='w-full h-full object-cover' />
+</div>
+   }
+    <div className='flex flex-col gap-0.5'>
+        <p className='font-semibold text-indigo-500 text-sm'>Pinned message #{index+1}</p>
+        <p className='text-[14px] '>{title}</p>
+    </div>
+</div>
+  }
+  </>
   )
 }
 
