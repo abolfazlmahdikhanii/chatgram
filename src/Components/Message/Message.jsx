e8654c80a0b0072baacff6c5a01128e4be335717
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-10-30 17:49:15 +0330

    modified for reply

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -3,6 +3,7 @@ import ProfileImage from '../ProfileImage/ProfileImage'
 import FileType from '../FileType/FileType'
 import FooterMessage from '../FooterMessage/FooterMessage'
 import { useLocation } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 
 const Message = ({
     from,
@@ -20,21 +21,23 @@ const Message = ({
     showCheck,
     edited,
     pin,
+    replyData,
+    hoverMessage,
+    hoverId,
 }) => {
     const location = useLocation()
     const hashId = location.hash.substring(1)
-    const [style,setStyle]=useState('')
+    const [style, setStyle] = useState('')
 
-    useEffect(()=>{
-     if(hashId === messageId){
-      setStyle('bg-indigo-300/10')
-      
-        setTimeout(() => {
-          setStyle("")
-         }, 800)
-      
-     }
-    },[location])
+    useEffect(() => {
+        if (hashId === messageId) {
+            setStyle('bg-indigo-300/10')
+
+            setTimeout(() => {
+                setStyle('')
+            }, 800)
+        }
+    }, [location])
 
     const formatTime = (date) => {
         return new Intl.DateTimeFormat('tr', {
@@ -46,19 +49,23 @@ const Message = ({
     const checkHandler = (id, check) => {
         onCheck(id, check)
     }
-  
+ 
+
     let arr = checkArr.findIndex((item) => item.messageId === messageId)
     return (
         <div
-     
-            className={`chat relative flex justify-between px-6 py-3 items-end ${from === 'user' ? 'chat-end' : 'chat-start'} 
-      ${checkArr[arr]?.check ? 'bg-indigo-300/10' : ''}  ${style } 
-      transition-all duration-200`}
+            className={`chat relative flex justify-between px-6 py-3 items-end ${
+                from === 'user' ? 'chat-end' : 'chat-start'
+            } 
+      ${
+          checkArr[arr]?.check ? 'bg-indigo-300/10' : ''
+      }  ${style}  transition-all duration-200`}
             onContextMenu={(e) => onContext(e, messageId)}
         >
+            {/* messageBody */}
             <div
-                id={messageId}
-                className={`chat-bubble break-words  ${
+                data-id={messageId}
+                className={`chat-bubble break-words px-2.5 ${
                     from === 'user' ? 'chat-bubble-primary' : 'chat-bubble'
                 }  ${
                     messageDis[0]?.type === 'file' ||
@@ -67,6 +74,28 @@ const Message = ({
                         : 'max-w-[420px] px-1.5 py-1.5'
                 } `}
             >
+                {/* box for reply Message */}
+
+                {replyData && (
+                    <HashLink
+                    to={`#${replyData.messageId}`}
+                    scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' }) }
+                    className="  mx-0 py-1 px-2 mb-2 w-32 rounded-lg flex gap-2.5 cursor-pointer transition-all duration-200  hover:bg-gray-600/30"
+                      
+                    >
+                        <p className="w-[2px] bg-gray-300 rounded-full "></p>
+                        <div className="flex flex-col gap-0.5">
+                            <p className="text-[14px] truncate text-white font-medium">
+                                {replyData.user}
+                            </p>
+                            <p className="text-[14px] truncate text-gray-100 font-normal">
+                                {replyData.messageDis}
+                            </p>
+                        </div>
+                    </HashLink>
+                )}
+
+                {/* message for Text */}
                 {typeof messageDis === 'string' ? (
                     <div
                         className="text-white"
