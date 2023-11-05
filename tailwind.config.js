45083527b9266e49a75a47637888cce34f0af080
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-05 16:17:26 +0330

    add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        fontFamily: {
+            'sans': 'Roboto-1, sans ,-apple-system, apple color emoji, BlinkMacSystemFont, Segoe UI, Oxygen-Sans, Ubuntu,Cantarell, Helvetica Neue',
+        },
         extend: {},
     },
 
