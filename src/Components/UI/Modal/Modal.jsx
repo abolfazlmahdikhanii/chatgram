861ffae91ccaaac960e1a993662f3255d837d119
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-05 16:21:53 +0330

    modal for forward

---
+++ src/Components/UI/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+import Backdrop from '../Backdrop/Backdrop'
+import UserProfile from '../../UserProfile/UserProfile'
+const Modal = ({ show ,chat }) => {
+    console.log(chat)
+    return (
+        <>
+           <Backdrop show={show} />
+            <dialog
+                id="my_modal_1"
+                className={`modal modal-box modal-bottom sm:modal-middle  justify-items-start [overflow-y:hidden] px-0 max-h-[700px] -translate-x-1/2 left-1/2 pt-0 mt-0 h-fit top-1/2 -translate-y-1/2 overflow-x-hidden ${
+                    show ? 'pointer-events-auto visible opacity-100' : ''
+                }`}
+            >
+                <div className="flex items-center gap-5 px-6 py-4 w-full">
+                    <button className="btn btn-sm btn-square   grid place-items-center">
+                        <AiOutlineClose size={16} />
+                    </button>
+                    <input type="text" className='input focus-visible:outline-0 text-xl font-normal text-white w-full' placeholder='Forward to ...' />
+                </div>
+
+                <div className="modal-body max-h-[350px] h-full min-h-[120px]">
+                    {/* body */}
+
+                    <ul className="  w-full  flex flex-col gap-3">
+                        <UserProfile/>
+                    </ul>
+                </div>
+            </dialog>
+        </>
+    )
+}
+
+export default Modal
