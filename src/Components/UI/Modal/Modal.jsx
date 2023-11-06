879fb08897f18718b837f199da4f75fb256bd2da
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-06 19:55:00 +0330

    modified

--- src/Components/UI/Modal/Modal.jsx
+++ src/Components/UI/Modal/Modal.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import Backdrop from '../Backdrop/Backdrop'
-import UserProfile from '../../UserProfile/UserProfile'
-const Modal = ({ show ,chat }) => {
-    console.log(chat)
+
+import ForwardList from '../../ForwardList/ForwardList'
+const Modal = ({ show, chat, messageID, setChat, userID,setShow }) => {
+
     return (
         <>
-           <Backdrop show={show} />
+            <Backdrop show={show} />
             <dialog
                 id="my_modal_1"
                 className={`modal modal-box modal-bottom sm:modal-middle  justify-items-start [overflow-y:hidden] px-0 max-h-[700px] -translate-x-1/2 left-1/2 pt-0 mt-0 h-fit top-1/2 -translate-y-1/2 overflow-x-hidden ${
@@ -14,17 +15,27 @@ const Modal = ({ show ,chat }) => {
                 }`}
             >
                 <div className="flex items-center gap-5 px-6 py-4 w-full">
-                    <button className="btn btn-sm btn-square   grid place-items-center">
+                    <button className="btn btn-sm btn-square   grid place-items-center" onClick={()=>setShow(false)}>
                         <AiOutlineClose size={16} />
                     </button>
-                    <input type="text" className='input focus-visible:outline-0 text-xl font-normal text-white w-full' placeholder='Forward to ...' />
+                    <input
+                        type="text"
+                        className="input focus-visible:outline-0 text-xl font-normal text-white w-full"
+                        placeholder="Forward to ..."
+                    />
                 </div>
 
                 <div className="modal-body max-h-[350px] h-full min-h-[120px]">
                     {/* body */}
 
-                    <ul className="  w-full  flex flex-col gap-3">
-                        <UserProfile/>
+                    <ul className="  w-full  flex flex-col gap-0.5">
+                        <ForwardList
+                            chat={chat}
+                            messageID={messageID}
+                            setChat={setChat}
+                            userID={userID}
+                            setShow={setShow}
+                        />
                     </ul>
                 </div>
             </dialog>
--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import Profile from '../Profile/Profile'
 
-const UserProfile = () => {
+const UserProfile = ({userName,relation,bgProfile,profileImg,activeStatus,onForward,id}) => {
+
     return (
-        <div className='user-profile hover:bg-gray-700/20'>
-            <Profile  size="m" />
+        <li className='user-profile hover:bg-gray-700/20' onClick={()=>onForward(id)}>
+           <Profile size="m" path={profileImg} userName={userName} bgProfile={bgProfile} relation={relation}/>     
 
             <div className="w-full flex-col gap-0.5 flex">
                 {/* top */}
           
                     <p className="  text-white capitalize text-[18px]">
-                        Abolfazl
+                        {userName}
                     </p>
                     <p className="text-[15px] text-gray-400">
-                        last seen recently
+                        {relation!=="me"?activeStatus:"forward here to save"}
                     </p>
                 
             </div>
-        </div>
+        </li>
     )
 }
 
