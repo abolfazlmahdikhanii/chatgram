d32120ad85d7e2fc288559dcda14b53ae6469018
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-06 19:53:29 +0330

    modified

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,63 +1,84 @@
-
-import React, { useEffect, useState } from "react";
-import SideMenu from "../SideMenu/SideMenu";
-import MessageList from "../MessageList/MessageList";
-import Chat from "../../Pages/Chat/Chat";
-import ChatInfo from "../ChatInfo/ChatInfo";
-import { Route, Routes, useParams } from "react-router-dom";
-
+import React, { useEffect, useState } from 'react'
+import SideMenu from '../SideMenu/SideMenu'
+import MessageList from '../MessageList/MessageList'
+import Chat from '../../Pages/Chat/Chat'
+import ChatInfo from '../ChatInfo/ChatInfo'
+import { Route, Routes, useParams } from 'react-router-dom'
 
 const Layout = () => {
-  const [chats, setChats] = useState([
-    {
-      id: 1,
-      userName: "Save",
-      
-      relation:"me",
-      profileImg:
-        "",
-      messages: [
 
-      ],
-    },
-    {
-      id: 2,
-      userName: "Abolfazl",
-      activeStatus:'last seen recently',
-      relation:"friend",
-      profileImg:
-        "https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg",
-      messages: [
 
-      ],
-    },
-    {
-      id: 3,
-      userName: "Elizabeth",
-      activeStatus:'last seen recently',
-      relation:"friend",
-      profileImg: "https://images.nightcafe.studio/jobs/X0DIQhUI5yfPMmykyDSi/X0DIQhUI5yfPMmykyDSi--4--a0vw0.jpg?tr=w-1600,c-at_max",
-      messages: [
-      
-      ],
-    },
-  ]);
-  const match = useParams();
-  return (
-    <div className={`${match.id?'grid-cols-[95px_340px_1fr_280px]':'grid-cols-[95px_340px_1fr]'} h-screen grid  overflow-hidden`}>
-      <SideMenu />
-      <MessageList chats={chats} />
-      <Routes>
-        <Route
-          path="/chat/:id"
-          element={<Chat chat={chats} setChat={setChats} />}
-        />
-      </Routes>
+const getRandomValue = () => {
+    const colors = [
+        'red',
+        'blue',
+        'green',
+        'yellow',
+        'indigo',
+        'purple',
+        'rose',
+        'violet'
+    ]
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
 
-     <ChatInfo />
 
-    </div>
-  );
-};
+    const [chats, setChats] = useState([
+        {
+            id: 1,
+            userName: 'AbMk',
+            
+            relation: 'me',
+            profileImg: 'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg',
+            bgProfile: 'violet',
+            messages: [],
+        },
+        {
+            id: 2,
+            userName: 'Abolfazl',
+            activeStatus: 'last seen recently',
+            relation: 'friend',
+            bgProfile: getRandomValue(),
+            profileImg:
+                'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg',
+            messages: [],
+        },
+        {
+            id: 3,
+            userName: 'Elizabeth',
+            activeStatus: 'last seen recently',
+            relation: 'friend',
+            bgProfile: getRandomValue(),
+            profileImg:
+                'https://images.nightcafe.studio/jobs/X0DIQhUI5yfPMmykyDSi/X0DIQhUI5yfPMmykyDSi--4--a0vw0.jpg?tr=w-1600,c-at_max',
+            messages: [],
+        },
+    ])
+   
 
-export default Layout;
+   
+    const match = useParams()
+    return (
+        <div
+            className={`${
+                match.id
+                    ? 'grid-cols-[95px_340px_1fr_280px]'
+                    : 'grid-cols-[95px_340px_1fr]'
+            } h-screen grid  overflow-hidden`}
+        >
+            <SideMenu />
+            <MessageList chats={chats} />
+            <Routes>
+                <Route
+                    path="/chat/:id"
+                    element={<Chat chat={chats} setChat={setChats} />}
+                />
+            </Routes>
+
+            <ChatInfo />
+        </div>
+    )
+}
+
+export default Layout
