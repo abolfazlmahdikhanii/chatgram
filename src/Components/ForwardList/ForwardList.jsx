8b31cb8a44f1198994a344bcde0aaa2733b69868
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-06 19:53:20 +0330

    show users

--- src/Components/ForwardList/ForwardList.jsx
+++ src/Components/ForwardList/ForwardList.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import UserProfile from '../userProfile/userProfile'
 
-const ForwardList = () => {
+const ForwardList = ({chat,messageID,setChat,userID,setShow}) => {
+  const [userMessage,setUserMessage]=useState()
+  const [userForwardMessage,setUserForwardMessage]=useState()
+
+  useEffect(()=>{
+    const findUserMessage=chat?.find((user)=>user?.id==userID)
+    setUserMessage(findUserMessage)
+  },[messageID,userMessage,setChat])
+
+console.log(messageID)
+  const forwardClickHandler=(id)=>{
+    const newChat=[...chat]
+    const findUserForward=newChat?.find((user)=>user.id===id)
+    
+    const findChat=userMessage?.messages.find((item)=>item.messageId===messageID)
+   
+    findUserForward.messages.push(findChat)
+    findUserForward.forward=userMessage
+    
+  setChat(newChat)
+  setShow(false)
+
+    
+  }
+
   return (
-    <div>ForwardList</div>
+    <>
+      {
+        chat.map((item)=>(
+          <UserProfile key={item.id} {...item} onForward={forwardClickHandler}/>
+        ))
+      }
+    </>
   )
 }
 
