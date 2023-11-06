d111e41f9e11fa49ded32fd63c7a2de5ec2ffd99
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-06 19:53:53 +0330

    set background for profile

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ProfileImage from "../ProfileImage/ProfileImage";
 
-const Profile = ({size,path,userName}) => {
+const Profile = ({size,path,userName,bgProfile,relation}) => {
 
   return (
     <div className="flex gap-5 ">
-      <ProfileImage size={size} src={path} userName={userName}/>
+      <ProfileImage size={size} src={path} userName={userName} bgProfile={bgProfile} relation={relation}/>
   
     </div>
   );
