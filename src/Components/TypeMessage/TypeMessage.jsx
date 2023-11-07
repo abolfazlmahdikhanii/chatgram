53658bd330302054bb205210133566d78da04cd5
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-07 21:11:04 +0330

    modified

--- src/Components/TypeMessage/TypeMessage.jsx
+++ src/Components/TypeMessage/TypeMessage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FcDocument } from 'react-icons/fc'
 
 const TypeMessage = ({ dis, w }) => {
     let element = null
@@ -14,15 +15,19 @@ const TypeMessage = ({ dis, w }) => {
         }
         if (dis[0]?.type === 'img') {
             element = (
-               <div>
-                 <div className={`${w ? w : 'w-11'} h-full mask mask-squircle `}>
-                    <img
-                        src={dis[0]?.src}
-                        alt=""
-                        className="w-full h-full object-cover"
-                    />
+                <div>
+                    <div
+                        className={`${
+                            w ? w : 'w-11'
+                        } h-full mask mask-squircle `}
+                    >
+                        <img
+                            src={dis[0]?.src}
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
                 </div>
-               </div>
             )
         }
         if (dis[0]?.type === 'video') {
@@ -42,11 +47,22 @@ const TypeMessage = ({ dis, w }) => {
                 </div>
             )
         }
-        if (
-            dis[0]?.type === 'file' ||
-            (dis[0]?.type === 'mp3' && dis[0]?.name !== '')
-        ) {
-            element = <p className="text-[14px] ">{dis[0]?.name}</p>
+        if (dis[0].type === 'file') {
+            element = (
+                <div className='flex gap-1.5 items-center w-full'>
+                    <p>
+                        <FcDocument />
+                    </p>
+                    <p className="text-[14px] truncate w-[40%]">
+                        {dis[0]?.name}
+                    </p>
+                </div>
+            )
+        }
+        if (dis[0]?.type === 'mp3' && dis[0]?.name !== '') {
+            element = (
+                <p className="text-[14px] truncate w-[50%]">{dis[0]?.name}</p>
+            )
         }
         if (dis[0]?.type === 'mp3' && dis[0]?.name === '') {
             element = <p className="text-[14px] ">Voice Message</p>
