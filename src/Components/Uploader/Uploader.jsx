465baeac4a08d07ea0fa8f792b72eb59ed05d099
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-17 22:04:24 +0330

    modified

--- src/Components/Uploader/Uploader.jsx
+++ src/Components/Uploader/Uploader.jsx
@@ -1,33 +1,40 @@
-import React from "react";
-import ModalPerview from "../UI/ModalPerview/ModalPerview";
-import ModalPerviewFile from "../UI/ModalPerviewFile/ModalPerviewFile";
-import Backdrop from "../UI/Backdrop/Backdrop";
+import React from 'react'
+import ModalPerview from '../UI/ModalPerview/ModalPerview'
+import ModalPerviewFile from '../UI/ModalPerviewFile/ModalPerviewFile'
+import Backdrop from '../UI/Backdrop/Backdrop'
 
 const Uploader = ({
-  showImage,
-  showFile,
-  closeImage,
-  closeFile,
-  files,
-  images,
-  remove,
-  onUploadImage,
-  onUploadFile,
-}) => {
-  return (
-    <div>
-      <Backdrop show={showFile ? showFile : showImage} />
-      <ModalPerviewFile
-        show={showFile}
-        close={closeFile}
-        files={files}
-        onRemove={remove}
-        onUpload={onUploadFile}
-      />
-      <ModalPerview show={showImage} close={closeImage} images={images}
-      onUpload={onUploadImage} />
-    </div>
-  );
-};
+    showImage,
+    showFile,
+    closeImage,
+    closeFile,
+    files,
+    images,
+    remove,
+    onUploadImage,
+    onUploadFile,
 
-export default Uploader;
+}) => {
+    return (
+        <div>
+            <Backdrop show={showFile ? showFile : showImage} />
+            <ModalPerviewFile
+                show={showFile}
+                close={closeFile}
+                files={files}
+                onRemove={remove}
+                onUpload={onUploadFile}
+             
+            />
+            <ModalPerview
+                show={showImage}
+                close={closeImage}
+                images={images}
+                onUpload={onUploadImage}
+             
+            />
+        </div>
+    )
+}
+
+export default Uploader
