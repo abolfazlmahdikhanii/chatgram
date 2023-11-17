005c3c91456ba555988d73845904504de0110add
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-17 22:03:09 +0330

    modified

--- src/Components/FooterMessage/FooterMessage.jsx
+++ src/Components/FooterMessage/FooterMessage.jsx
@@ -3,7 +3,7 @@ import { BiCheckDouble } from "react-icons/bi";
 import { BiCheck } from "react-icons/bi";
 import { BsFillPinAngleFill } from "react-icons/bs";
 import { Watch } from "react-loader-spinner";
-const FooterMessage = ({message,date,read,send,edited,pin}) => {
+const FooterMessage = ({message,date,read,send,edited,pin,reaction}) => {
   let icon = null;
   if (read) {
     icon = <BiCheckDouble size={14} color="" />;
@@ -26,7 +26,7 @@ const FooterMessage = ({message,date,read,send,edited,pin}) => {
 
   return (
     <>
-      {message?.type === "file"||message?.type === "mp3" || typeof message === "string" ? (
+      {message?.type === "file"||message?.type === "mp3" || typeof message === "string"||reaction||message.caption ? (
         <div className=" flex items-center gap-1 mt-1 px-3 justify-end chat-footer -mr-4">
           <p className="text-gray-500 text-[12px] mr-1.5">{pin?<BsFillPinAngleFill size={12}/>:''}</p>
           <p className="text-gray-400 text-[12px] mr-0.5 italic">{edited?'edited':''}</p>
@@ -35,8 +35,8 @@ const FooterMessage = ({message,date,read,send,edited,pin}) => {
           {icon}
         </div>
       ) : (
-        <div className=" flex items-center gap-1  px-1.5 absolute bottom-2 right-4 bg-gray-700/60 rounded-lg py-[1px]">
-          <p className="text-gray-200 text-[10px]">{date}</p>
+        <div className={` flex items-center gap-1  px-1.5 absolute ${!reaction?"bottom-2":"bottom-[21%]"} bottom-2 right-4 bg-gray-700/60 rounded-lg py-[1px] mt-2`}>
+          <p className="text-gray-200 text-[10px] font-sans">{date}</p>
         
           {icon}
         </div>
