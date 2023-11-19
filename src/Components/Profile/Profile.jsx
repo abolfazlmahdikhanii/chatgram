d0dc7b697cd8067217a0ca71a3f66d34ef3ab39d
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-19 17:31:34 +0330

    modified

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ProfileImage from "../ProfileImage/ProfileImage";
 
-const Profile = ({size,path,userName,bgProfile,relation}) => {
+const Profile = ({size,path,userName,bgProfile,relation,isSave}) => {
 
   return (
     <div className="flex gap-5 ">
-      <ProfileImage size={size} src={path} userName={userName} bgProfile={bgProfile} relation={relation}/>
+      <ProfileImage size={size} src={path} userName={userName} bgProfile={bgProfile} relation={relation} isSave={isSave}/>
   
     </div>
   );
