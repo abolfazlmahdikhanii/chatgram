cd54bf985f2b4f17d6dd4ea7d9098998f0556679
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-19 17:30:50 +0330

    modified profile image

--- src/Components/ChatHeader/ChatHeader.jsx
+++ src/Components/ChatHeader/ChatHeader.jsx
@@ -23,7 +23,7 @@ const HeaderMessage = ({info}) => {
     return (
       <section className="px-5 flex items-center justify-between">
             <div className="flex gap-4">
-                <Profile path={info?.profileImg}  userName={info?.userName} bgProfile={info?.bgProfile} relation={info?.relation}/>
+                <Profile path={info?.profileImg}  userName={info?.userName} bgProfile={info?.bgProfile} relation={info?.relation} isSave={true}/>
                 <div className="w-full flex-col   flex">
                     <p className="font-semibold  text-white capitalize text-[17px]">
                         {info?.userName}
