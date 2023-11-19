9af108273fc46982fb9b5b7e2ea3335f98b74347
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-19 17:31:42 +0330

    modified

--- src/Components/ProfileImage/ProfileImage.jsx
+++ src/Components/ProfileImage/ProfileImage.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import {BsFillBookmarkFill} from "react-icons/bs"
-const ProfileImage = ({ size, src, userName, bgProfile,relation }) => {
+const ProfileImage = ({ size, src, userName, bgProfile,relation,isSave=false }) => {
     return (
         <div className="avatar">
             <div
-                className={`mask mask-squircle ${
-                    size === 'm' ? 'w-14 h-14' : 'w-11 h-11'
-                }`}
+                className={`mask mask-squircle w-11 h-11 ${
+                    size === 'm' && 'w-14 h-14'
+                } ${size==="xs"&&'w-5 h-5'}`
+                
+            }
             >
-                {src&&relation!=="me" ? (
+                {src &&!isSave? (
                     <img src={src} alt="profile" className="w-full h-full" />
                 ) : (
                     <div
                         data-color={bgProfile}
                         className={`grid place-items-center text-white h-full font-bold w-full `}
                     >
-                        {relation!=="me"?<span>{userName && userName[0]}</span>:<span><BsFillBookmarkFill size={size==="m"?22:16} color='#fff'/></span>}
+                        {!src||!isSave?<span>{userName && userName[0]}</span>:<span><BsFillBookmarkFill size={size==="m"?22:16} color='#fff'/></span>}
                     </div>
                 )}
             </div>
