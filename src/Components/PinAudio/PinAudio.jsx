7cf9f7f4212f709dd15f5bf58b47824a87a5222b
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-24 16:38:59 +0330

    modified

--- src/Components/PinAudio/PinAudio.jsx
+++ src/Components/PinAudio/PinAudio.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useContext } from 'react'
+import React, { useState, useRef, useEffect, useContext, useMemo } from 'react'
 import { FaBackward } from 'react-icons/fa6'
 import { FaPlay } from 'react-icons/fa6'
 import { FaForward } from 'react-icons/fa6'
@@ -6,43 +6,50 @@ import { FaPause } from 'react-icons/fa'
 import { IoClose } from 'react-icons/io5'
 import { MusicControlContext } from '../../Context/MusicContext'
 
-const PinAudio = ({ path }) => {
-    const [check,setCheck]=useState(false)
-    const { isPlay, currentSong, currentTimeMusic, playMusic, pauseMusic, seekMusic,durationMusic,containerBox,playbackRateMusic } = useContext(MusicControlContext);
+const PinAudio =() => {
+    const [check, setCheck] = useState(false)
+    const {
+        isPlay,
+        currentSong,
+        currentTimeMusic,
+        playMusic,
+        pauseMusic,
+        seekMusic,
+        durationMusic,
+        containerBox,
+        playbackRateMusic,
+
+    } = useContext(MusicControlContext)
+
+ 
+
+ 
+
+ 
 
 
-    const togglePlay = () => {   
-            playMusic(currentSong,containerBox,durationMusic)
+    const togglePlay = () => {
+        playMusic(currentSong, containerBox)
     }
-
-   
 
     const handlePlayBackRate = (e) => {
         e.target.checked
-            ? (playbackRateMusic(2,true))
-            : (playbackRateMusic(1,false))
+            ? playbackRateMusic(2, true)
+            : playbackRateMusic(1, false)
     }
 
     return (
         <div className="py-2 px-4 bg-base-200  flex items-center  justify-between sticky top-0 relative">
             <div className="flex items-center gap-2  justify-between w-full">
                 <div className="flex items-center gap-x-0.5">
-                    <button className="btn text-indigo-600 ">
-                        <FaBackward size={19} />
-                    </button>
+                    
                     <button
                         className="btn text-indigo-600 "
                         onClick={togglePlay}
                     >
-                        {!isPlay ? (
-                            <FaPlay size={18} />
-                        ) : (
-                            <FaPause size={18} />
-                        )}
+                        {isPlay ? <FaPlay size={18} /> : <FaPause size={18} />}
                     </button>
-                    <button className="btn text-indigo-600 ">
-                        <FaForward size={19} />
-                    </button>
+             
                 </div>
                 <div className="flex items-center gap-0.5">
                     <label htmlFor="chk" className=" btn text-lg relative ">
@@ -69,24 +76,17 @@ const PinAudio = ({ path }) => {
                 </div>
             </div>
             <div
-                className="absolute -bottom-2 left-0 "
-                style={{
-                    width: `${
-                        (currentTimeMusic / durationMusic) * 100
-                    }%`,
-                }}
+                className="absolute -bottom-0 left-0 w-full cursor-pointer"
+            
             >
-                <progress
+                <div
                     // onClick={handleProgressBarClick}
-                    className="h-1 border-[1px] progress-primary  progress  w-full hover:border-[3px] transition-all duration-300"
-                    value={`${
-                        (currentTimeMusic /durationMusic) * 100
-                    }`}
-                    max="100"
-                ></progress>
+                    className="h-1  hover:h-1.5  bg-indigo-600 w-0 block cursor-pointer"
+                    style={{width:(currentTimeMusic / durationMusic) * 100+"%" }}
+                    
+                ></div>
             </div>
 
-           
         </div>
     )
 }
