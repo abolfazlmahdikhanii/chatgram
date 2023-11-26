722278b039b3f15b8b090e6f971d419041501338
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-26 19:48:28 +0330

    modified

--- src/Components/UI/ReactionBox/ReactionBox.jsx
+++ src/Components/UI/ReactionBox/ReactionBox.jsx
@@ -3,7 +3,7 @@ import Profile from '../../Profile/Profile'
 
 const ReactionBox = ({reaction,setReaction}) => {
     return (
-        <div className='bg-gray-300/30  px-2 py-1  rounded-[10px] flex items-center justify-between my-2.5 w-[70px] ml-auto backdrop-blur-md cursor-pointer' onClick={()=>setReaction(false)}>
+        <div className='bg-gray-300/30  px-2 py-1  rounded-lg flex items-center justify-between my-2.5 w-[70px] ml-auto backdrop-blur-md cursor-pointer' onClick={()=>setReaction(false)}>
             <div>
             <Profile path={reaction?.profile.profileImg}  userName={reaction?.profile.userName} bgProfile={reaction?.profile.bgProfile} relation={reaction?.profile.relation} size="xs"/>
             </div>
