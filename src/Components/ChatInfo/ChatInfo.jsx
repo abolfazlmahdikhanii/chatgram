ec84b97caa2e942c6ca6501e8752845e54db20ca
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-11-27 21:14:28 +0330

    add new component for show info of user

--- src/Components/ChatInfo/ChatInfo.jsx
+++ src/Components/ChatInfo/ChatInfo.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Box from '../UI/Box/Box'
 import Profile from '../Profile/Profile'
 import { IoCallSharp } from 'react-icons/io5'
-import { MdAlternateEmail } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
 
-const ChatInfo = ({ info }) => {
+import { NavLink } from 'react-router-dom'
+import InfoBox from './InfoBox'
+import FileType from '../FileType/FileType'
+
+const ChatInfo = ({ info, chat,setChatInfo }) => {
+    const [tab, setTab] = useState('Media')
+    const [type, setType] = useState()
+    useEffect(() => {
+        if (info) {
+            filterShared('img', 'video')
+        }
+    }, [info, chat])
+
+    const filterShared = (type, type1 = null) => {
+        const newMessages = [...info.messages]
+        const newData = []
+        // const newMessagesDis=[...newMessages.messageDis]
+        for (const item of newMessages) {
+            for (const val of item.messageDis) {
+                if (val.type === type || (type1 && val.type === type1))
+                    newData.push(val)
+            }
+        }
+        setType(newData)
+        // console.log(filterByType)
+    }
+
     return (
-        <Box style="w-full transition-all duration-200 overflow-y-scroll h-[89%] n-scroll">
+        <Box style="w-full transition-all duration-200 overflow-y-scroll h-full n-scroll max-h-[100vh] relative">
+            {/* header */}
+            <div className="sticky -top-4 bg-base-100 z-10  py-4 -mt-3 w-full px-2 transition-all duration-200 flex items-center justify-between">
+                <p className="text-xl text-white font-semibold">Profile</p>
+                <button className='btn btn-ghost mask mask-squircle' onClick={()=>setChatInfo(false)}>
+                   <IoMdClose size={20}/>
+                </button>
+            </div>
             <div className="mt-14  flex flex-col gap-5 items-center justify-center ">
                 <div className=" flex items-center justify-center">
                     <Profile
@@ -23,21 +56,21 @@ const ChatInfo = ({ info }) => {
                         {info?.userName}
                     </p>
                     <p className="text-center text-sm text-indigo-400">
-                        Online{' '}
+                        Online
                     </p>
                 </div>
                 {/* btn */}
-                <div className="px-3 flex items-center gap-4 w-full justify-center">
-                    <button className=" bg-green-500 text-white btn  rounded-[15px] hover:bg-green-600   pr-8 min-h-[3.3rem]">
-                        <IoCallSharp size={24} />
+                <div className="px-3 flex items-center gap-4 w-full justify-center mt-3.5">
+                    <button className=" bg-green-500 text-white btn  rounded-[15px] hover:bg-green-600   pr-8 min-h-[3.2rem]">
+                        <IoCallSharp size={22} />
                         <p className="text-base capitalize font-semibold">
                             Call
                         </p>
                     </button>
-                    <button className=" bg-indigo-600 text-white btn  rounded-[15px] hover:bg-indigo-700   pr-8 gap-2.5 min-h-[3.3rem]">
+                    <button className=" bg-blue-700 text-white btn  rounded-[15px] hover:bg-blue-600   pr-8 gap-2.5 min-h-[3.2rem]">
                         <svg
-                            width={24}
-                            height={24}
+                            width={22}
+                            height={22}
                             viewBox="0 0 24 24"
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
@@ -64,94 +97,94 @@ const ChatInfo = ({ info }) => {
                 </div>
             </div>
 
-{/* info */}
+            {/* info */}
             <div className="mt-7 px-2.5 space-y-3">
                 {/* item-1 */}
-                <div className="info-box">
-                    {/* head */}
-                    <div className="flex  gap-x-3">
-                        <p className="text-amber-500">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeMiterlimit="10"
-                                    strokeWidth="1.5"
-                                    d="M17 20.5H7c-3 0-5-1.5-5-5v-7c0-3.5 2-5 5-5h10c3 0 5 1.5 5 5v7c0 3.5-2 5-5 5z"
-                                ></path>
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeMiterlimit="10"
-                                    strokeWidth="1.5"
-                                    d="M17 9l-3.13 2.5c-1.03.82-2.72.82-3.75 0L7 9"
-                                ></path>
-                            </svg>
-                        </p>
-                        <div className="flex flex-col gap-1">
-                            <p className=" text-gray-500 text-sm font-medium">Email</p>
-                            <p className="  text-gray-50 px-1.5 font-normal">ab@example.com</p>
-                        </div>
-                    </div>
-                </div>
+                <InfoBox title="Email" des="ab@example.com" />
                 {/* item-2 */}
-                <div className="info-box">
-                    {/* head */}
-                    <div className="flex  gap-x-3">
-                        <p className="text-green-500">
-                            <MdAlternateEmail size={23}/>
-                        </p>
-                        <div className="flex flex-col gap-1">
-                            <p className=" text-gray-500 text-sm font-medium">Username</p>
-                            <p className="line-clamp-3  text-gray-50 px-1.5 font-normal">abmk</p>
-                        </div>
-                    </div>
-                </div>
+                <InfoBox title="Username" des={info?.userName} />
                 {/* item-3 */}
-                <div className="info-box">
-                    {/* head */}
-                    <div className="flex  gap-x-3">
-                        <p className="text-indigo-600">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    fillRule="evenodd"
-                                    d="M16.334 2C19.723 2 22 4.378 22 7.916v8.168C22 19.622 19.723 22 16.332 22H7.664C4.276 22 2 19.622 2 16.084V7.916C2 4.378 4.276 2 7.664 2h8.67zm0 1.5h-8.67C5.135 3.5 3.5 5.233 3.5 7.916v8.168c0 2.683 1.635 4.416 4.164 4.416h8.668c2.532 0 4.168-1.733 4.168-4.416V7.916c0-2.683-1.636-4.416-4.166-4.416zm-4.34 7.75a.75.75 0 01.75.75v4a.75.75 0 01-1.5 0v-4a.75.75 0 01.75-.75zm.005-4.046a1 1 0 110 2 1.003 1.003 0 01-1.005-1c0-.553.443-1 .995-1h.01z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
-                        </p>
-                        <div className="flex flex-col gap-1">
-                            <p className=" text-gray-500 text-sm font-medium">Bio</p>
-                            <p className="line-clamp-3  text-gray-50 px-1.5 font-normal">
-                                Lorem, ipsum dolor sit amet consectetur
-                                adipisicing elit. Aliquam suscipit similique eos
-                                sit voluptates atque eum harum ad minus commodi
-                                recusandae rerum soluta reprehenderit
-                                dignissimos repellat, placeat in nemo rem?
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <InfoBox
+                    title="Bio"
+                    des="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    style="line-clamp-2"
+                />
             </div>
 
-            {/* media */}
-            <div>
-              
+            {/* shared */}
+            <div className="mt-9 px-2.5">
+                <p className="text-lg text-gray-100 font-semibold mb-5 px-1 sticky top-0">
+                    Shared Media
+                </p>
+                <div
+                    role="tablist"
+                    className="tabs tabs-boxed p-2 transition-all duration-200 gap-x-0.5"
+                >
+                    <button
+                        role="tab"
+                        className={`tab h-[2.2rem]  transition-all duration-200 ${
+                            tab === 'Media' ? 'tab-active' : ''
+                        }`}
+                        onClick={(e) => {
+                            setTab(e.target.textContent)
+                            filterShared('img', 'video')
+                        }}
+                    >
+                        Media
+                    </button>
+                    <button
+                        role="tab"
+                        className={`tab  h-[2.2rem] transition-all duration-200  ${
+                            tab === 'Files' ? 'tab-active' : ''
+                        }`}
+                        onClick={(e) => {
+                            setTab(e.target.textContent)
+                            filterShared('file')
+                        }}
+                    >
+                        Files
+                    </button>
+                    <button
+                        role="tab"
+                        className={`tab h-[2.2rem] transition-all duration-200  ${
+                            tab === 'Links' ? 'tab-active' : ''
+                        }`}
+                        onClick={(e) => {
+                            setTab(e.target.textContent)
+                            filterShared('links')
+                        }}
+                    >
+                        Links
+                    </button>
+                    <button
+                        role="tab"
+                        className={`tab h-[2.2rem] transition-all duration-200  ${
+                            tab === 'Voice' ? 'tab-active' : ''
+                        }`}
+                        onClick={(e) => {
+                            setTab(e.target.textContent)
+                            filterShared('mp3')
+                        }}
+                    >
+                        Voice
+                    </button>
+                </div>
+                {/* shared-body */}
+                <section>
+                    {/* media */}
+                    <div className="grid grid-cols-3 gap-1.5   mt-6  overflow-hidden ">
+                        {type &&
+                            type.map((content) => (
+                                <FileType
+                                    key={content.id}
+                                    {...content}
+                                    autoPlay={false}
+                                    imgSize={true}
+                                    isFile={false}
+                                />
+                            ))}
+                    </div>
+                </section>
             </div>
         </Box>
     )
