c0d95aa7cc8c01c300091a2f58f42445659bc5b0
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-01 00:07:24 +0330

    modified

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -550,7 +550,7 @@ const Chat = ({ chat, setChat }) => {
                 )}
             </div>
 
-            {ChatInfo && <ChatInfo info={message} chat={chat} setChatInfo={setShowChatInfo}/>}
+            {showChatInfo && <ChatInfo info={message} chat={chat} setChatInfo={setShowChatInfo}/>}
         </div>
     )
 }
