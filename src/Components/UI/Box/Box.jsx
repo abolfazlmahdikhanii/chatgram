e0db802bc3068691bdbaec78f7c103204ae45fa4
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-01 00:07:18 +0330

    modified

--- src/Components/UI/Box/Box.jsx
+++ src/Components/UI/Box/Box.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Box = ({children,style}) => {
-  return (
-    <div className={`dark:bg-base-100 px-2 py-4 ${style}`}>
-      {children}
-    </div>
-  )
+const Box = ({ children, style }) => {
+
+    return (
+        <div
+            className={`dark:bg-base-100 px-2 py-4 flex flex-col ${style}  transition-all duration-200`}
+            
+        >
+            {children}
+        </div>
+    )
 }
 
 export default Box
