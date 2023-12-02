4655cf67a332fd9700bd40c61522a8456becd823
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-02 20:28:49 +0330

    modified

--- src/Components/UI/Box/Box.jsx
+++ src/Components/UI/Box/Box.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Box = ({ children, style }) => {
+const Box = ({ children, style,context }) => {
 
     return (
         <div
             className={`dark:bg-base-100 px-2 py-4 flex flex-col ${style}  transition-all duration-200`}
-            
+            onContextMenu={context}
         >
             {children}
         </div>
