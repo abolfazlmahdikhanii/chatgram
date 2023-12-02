a183d1dd8cfce9efc0cb6a9caf7bd9682ed6002e
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-02 20:28:54 +0330

    modified

---
+++ src/Components/UI/CoustomToast/CoustomToast.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IoCopyOutline } from "react-icons/io5";
+const CoustomToast = ({message}) => {
+  return (
+    <div className='flex items-center gap-3 -3xl '>
+    <p>
+       <IoCopyOutline size={22}/>
+    </p>
+    <p>{message} copied to clipboard.</p>
+   </div>
+  )
+}
+
+export default CoustomToast
