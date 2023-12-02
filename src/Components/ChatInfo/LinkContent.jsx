ba89ae343c96053f5a6b08537d530724d2ce898f
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-02 20:28:26 +0330

    component for display link

--- src/Components/ChatInfo/LinkContent.jsx
+++ src/Components/ChatInfo/LinkContent.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { FiLink } from "react-icons/fi";
 
-const LinkContent = ({link}) => {
+const LinkContent = ({link,contextMenu,id}) => {
+ 
+const showHostName=(links)=>{
+  const url = new URL(links);
+return url.hostname; 
+}
+
+
   return (
-    <li  className='flex gap-2.5 items-center transition-all py-2.5 px-3 rounded-xl duration-200 hover:bg-gray-700/30 cursor-pointer'>
+    <div  className='flex gap-3 items-center transition-all py-2.5 px-3 rounded-xl duration-200 hover:bg-gray-700/30 cursor-pointer'
+    onContextMenu={(e)=>contextMenu(e,id)}
+    >
         <div data-color='violet' className=' h-[70px] mask mask-squircle bg-primary w-[70px] grid place-items-center '>
           <FiLink size={25} color='#fff'/>
         </div>
         <div className='max-w-[70%] space-y-1.5'>
-            <p className='text-sm truncate text-gray-50'>www.aparat.com</p>
+            <p className='text-sm truncate text-gray-50'>{showHostName(link)}</p>
             <a href={link} target='_blank' rel='noreferrer noopener' className='truncate text-sm text-indigo-400 max-w-[90%] line-clamp-1 block'>{link}</a>
         </div>
-    </li>
+    </div>
   )
 }
 
