f7f46d6a6e1d0e404f12ae7785520065ba87c1ec
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-03 18:10:48 +0330

    modified

--- src/Components/ChatInfo/LinkContent.jsx
+++ src/Components/ChatInfo/LinkContent.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
-import { FiLink } from "react-icons/fi";
+import { FiLink } from 'react-icons/fi'
 
-const LinkContent = ({link,contextMenu,id}) => {
- 
-const showHostName=(links)=>{
-  const url = new URL(links);
-return url.hostname; 
+const LinkContent = ({ link, contextMenu, id, remove }) => {
+    const showHostName = (links) => {
+
+        if (!links.startsWith('www')) {
+            const url = new URL(links)
+            return url?.hostname
+        }
+        else return links
+    }
+
+    return (
+        <div
+            className="flex gap-3 items-center transition-all py-2.5 px-3 rounded-xl duration-200 hover:bg-gray-700/30 cursor-pointer"
+            onContextMenu={(e) => contextMenu(e, id, true)}
+        >
+            <div
+                data-color="violet"
+                className=" h-[70px] mask mask-squircle bg-primary w-[70px] grid place-items-center "
+            >
+                <FiLink size={25} color="#fff" />
+            </div>
+            <div className="max-w-[70%] space-y-1.5">
+                <p className="text-sm truncate text-gray-50">
+                    {showHostName(link&&link[0])}
+                </p>
+                <a
+                    href={link&&link[0].startsWith("www")?`https://${link[0]}`:link[0]}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="truncate text-sm text-indigo-400 max-w-[90%] line-clamp-1 block"
+                >
+                    {link&&link[0].startsWith("www")?`https://${link[0]}`:link[0]}
+                </a>
+            </div>
+        </div>
+    )
 }
 
-
-  return (
-    <div  className='flex gap-3 items-center transition-all py-2.5 px-3 rounded-xl duration-200 hover:bg-gray-700/30 cursor-pointer'
-    onContextMenu={(e)=>contextMenu(e,id)}
-    >
-        <div data-color='violet' className=' h-[70px] mask mask-squircle bg-primary w-[70px] grid place-items-center '>
-          <FiLink size={25} color='#fff'/>
-        </div>
-        <div className='max-w-[70%] space-y-1.5'>
-            <p className='text-sm truncate text-gray-50'>{showHostName(link)}</p>
-            <a href={link} target='_blank' rel='noreferrer noopener' className='truncate text-sm text-indigo-400 max-w-[90%] line-clamp-1 block'>{link}</a>
-        </div>
-    </div>
-  )
-}
-
-export default LinkContent 
+export default LinkContent
