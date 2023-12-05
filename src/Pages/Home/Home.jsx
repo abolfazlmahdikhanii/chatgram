7588fe399e8eb827bfc8fa8b8aaa8f7dbcb1cc70
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-05 22:55:41 +0330

    modified

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-9/12 flex flex-col items-center justify-center mx-auto h-screen gap-16'>
+    <div>
+        <img src="search-chat.svg" alt="" />
+    </div>
+    <p className='text-2xl text-white'>Choose one of the chats to start</p>
+    </div>
+  )
+}
+
+export default Home
