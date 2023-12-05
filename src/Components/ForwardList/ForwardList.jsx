66b7531d434a1ac54a9ae93949434e3a843ff241
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-05 22:55:03 +0330

    modified

--- src/Components/ForwardList/ForwardList.jsx
+++ src/Components/ForwardList/ForwardList.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import UserProfile from '../userProfile/userProfile'
 
-const ForwardList = ({chat,messageID,setChat,userID,setShow,onForward}) => {
+const ForwardList = ({chat,messageID,setChat,userID,setShow,onForward,onForwardContact,setForwardContact,forwardContact}) => {
 
 
   return (
     <>
       {
         chat?.map((item)=>(
-          <UserProfile key={item.id} {...item} onForward={()=>onForward(item.id)}/>
+          <UserProfile key={item.id} {...item} onForward={()=>{
+            !forwardContact? onForward(item.id):onForwardContact(item.id)
+            setForwardContact(false)
+          }} />
         ))
       }
     </>
