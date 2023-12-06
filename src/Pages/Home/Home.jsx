cd3d681f5c24dea46bc71e0f170574aea5d6f1cf
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-06 12:46:17 +0330

    modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-9/12 flex flex-col items-center justify-center mx-auto h-screen gap-16'>
+    <div className="w-8/12 flex flex-col items-center justify-center mx-auto h-screen gap-16 p-4 ">
     <div>
         <img src="search-chat.svg" alt="" />
     </div>
-    <p className='text-2xl text-white'>Choose one of the chats to start</p>
+    <p className='text-2xl text-white '>Choose one of the chats to start</p>
     </div>
   )
 }
