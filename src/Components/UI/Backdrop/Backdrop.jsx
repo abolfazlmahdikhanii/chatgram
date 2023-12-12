760a7c8e1ce409ef830001802a10a772f28c2673
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-12-12 20:38:58 +0330

    modified

--- src/Components/UI/Backdrop/Backdrop.jsx
+++ src/Components/UI/Backdrop/Backdrop.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Backdrop = ({show,close}) => {
+const Backdrop = ({show,close,preview}) => {
 
   return (
-    <div className={`h-screen w-full bg-base-200/80  fixed inset-0 z-20 backdrop:backdrop-blur-xl  ${show?'block':'hidden'}`}
+    <div className={`h-screen w-full ${preview?'bg-base-200/95':'bg-base-200/80'}  fixed inset-0 z-20 backdrop:backdrop-blur-xl  ${show?'block':'hidden'}`}
     onClick={close}
     >
 
