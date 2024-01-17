c72157795b7137d229af1cd7c84c33f4445ce610
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-17 16:26:05 +0330

    fix bug

--- src/Components/MessageList/MessageList.jsx
+++ src/Components/MessageList/MessageList.jsx
@@ -1,27 +1,56 @@
-import React from "react";
-import Box from "../UI/Box/Box";
-import MessageItem from "../Messageitem/MessageItem";
+import React, { useContext, useState } from 'react'
+import Box from '../UI/Box/Box'
+import MessageItem from '../Messageitem/MessageItem'
+import { ChatContext } from '../../Context/ChatContext'
+import UserMenu from './UserMenu'
+import {useNavigate} from 'react-router-dom'
 
-const MessageList = ({chats}) => {
+const MessageList = () => {
+    const [chatId, setChatId] = useState(null)
+    const [pageX, setPageX] = useState(null)
+    const [pageY, setPageY] = useState(null)
+    const [showMenu, setShowMenu] = useState(false)
+    const { chat, DeleteChat } = useContext(ChatContext)
+    const navigate=useNavigate()
 
- 
+    const contextMenuHandler = (e, id) => {
+        e.preventDefault()
+        setShowMenu((prev) => !prev)
+        setPageX(e.pageX)
+        setPageY(e.pageY)
+        setChatId(id)
+    }
 
-  return (
-    <Box style={"ml-3"}>
-       <div className="py-1 px-2 mb-5">
-        <h2 className="font-bold text-2xl text-white">Message</h2>
-       </div>
-      <div className="w-full overflow-hidden space-y-2 h-screen overflow-y-auto ">
-    
-        {
-          chats?.map((chat)=>(
-            <MessageItem key={chat.id} {...chat} isSave={chat.relation==="me"?true:false}/>
-          ))
-        }
-    
-      </div>
-    </Box>
-  );
-};
+    return (
+        <Box style={'ml-3'}>
+            <div className="py-1 px-2 mb-5">
+                <h2 className="font-bold text-2xl dark:text-white text-gray-800">
+                    Message
+                </h2>
+            </div>
+            <div className="w-full overflow-hidden space-y-2 h-screen overflow-y-auto ">
+                {chat?.map((chat) => (
+                    <MessageItem
+                        key={chat.id}
+                        {...chat}
+                        isSave={chat.relation === 'me' ? true : false}
+                        onContext={(e) => contextMenuHandler(e, chat.id)}
+                    />
+                ))}
+            </div>
+            <UserMenu
+                show={showMenu}
+                pageX={pageX}
+                pageY={pageY}
+                chatId={chatId}
+                closeMenu={setShowMenu}
+                deleteChat={()=>{
+                  DeleteChat(chatId)
+                  navigate('/')
+                }}
+            />
+        </Box>
+    )
+}
 
-export default MessageList;
+export default MessageList
