95ed6f3edf66c45387a93b97625bb998c32dbb3a
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-17 16:33:10 +0330

    modified

--- src/Components/UI/ActionModal/ActionModal.jsx
+++ src/Components/UI/ActionModal/ActionModal.jsx
@@ -5,7 +5,7 @@ const ActionModal = ({click}) => {
   return (
     <div className=" pl-2  w-full flex items-center justify-between gap-3 pr-5   pt-3">
       <textarea
-        className="input focus:outline-none  w-full  resize-none  text-white bg-transparent "
+        className="input focus:outline-none  w-full  resize-none  dark:text-white bg-transparent text-gray-800"
         placeholder="Add a Caption"
         onChange={(e)=>setCaption(e.target.value)}
         value={caption}
