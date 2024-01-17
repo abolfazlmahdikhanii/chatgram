5322fc1cc7e79309315fd2df88edcf63eadf17f9
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-17 16:24:38 +0330

    modified for light mode

--- src/Components/ChatHeader/ChatHeader.jsx
+++ src/Components/ChatHeader/ChatHeader.jsx
@@ -1,39 +1,65 @@
-import React,{useState} from 'react'
+import React, { useContext, useState } from 'react'
 import { BiDotsHorizontalRounded, BiDotsVerticalRounded } from 'react-icons/bi'
 import Box from '../UI/Box/Box'
 import Profile from '../Profile/Profile'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { TbSquareRoundedCheck } from 'react-icons/tb'
+import { ChatContext } from '../../Context/ChatContext'
+import { MdOutlineAccessTime } from 'react-icons/md'
 
-const ChatHeader = ({
-    info,
-    showPin,
-    pinMessage,
-    setShowPin,
-    setShowChatInfo,
-    setCheckBox,
-    showCheckBox,
-    DeleteChat,
-    setCheckMessage,
-    forwardContact
-    
-}) => {
+const ChatHeader = ({ setShowChatInfo, DeleteChat }) => {
+    const [showMenu, setShowMenu] = useState(false)
+    const {
+        message,
+        showCheckBox,
+        ForwardContactHandler,
+        pinMessage,
+        setShowCheckBox,
+        setCheckMessage,
+        showPin,
+        showPinHandler,
+        chatId,
+       clearHistory
+    } = useContext(ChatContext)
 
-    const [showMenu,setShowMenu]=useState(false)
     return (
         <Box>
             {!showPin ? (
-                <HeaderMessage info={info} setShowChatInfo={setShowChatInfo} showMenu={showMenu} setShowMenu={setShowMenu} setCheckBox={setCheckBox} showCheckBox={showCheckBox} DeleteChat={DeleteChat} setCheckMessage={setCheckMessage} forwardContact={forwardContact}/>
+                <HeaderMessage
+                    info={message}
+                    setShowChatInfo={setShowChatInfo}
+                    showMenu={showMenu}
+                    setShowMenu={setShowMenu}
+                    setCheckBox={setShowCheckBox}
+                    showCheckBox={showCheckBox}
+                    DeleteChat={DeleteChat}
+                    setCheckMessage={setCheckMessage}
+                    forwardContact={ForwardContactHandler}
+                    userId={chatId}
+                    clearHistory={clearHistory}
+                />
             ) : (
                 <HeaderPinMessage
                     pinMessage={pinMessage}
-                    setShowPin={setShowPin}
+                    setShowPin={showPinHandler}
                 />
             )}
         </Box>
     )
 }
-const HeaderMessage = ({ info, setShowChatInfo,showMenu,setShowMenu,setCheckBox,showCheckBox,DeleteChat,setCheckMessage,forwardContact }) => {
+const HeaderMessage = ({
+    info,
+    setShowChatInfo,
+    showMenu,
+    setShowMenu,
+    setCheckBox,
+    showCheckBox,
+    DeleteChat,
+    setCheckMessage,
+    forwardContact,
+    userId,
+    clearHistory
+}) => {
     return (
         <section className="px-5 flex items-center justify-between relative">
             <div className="flex gap-4">
@@ -45,10 +71,10 @@ const HeaderMessage = ({ info, setShowChatInfo,showMenu,setShowMenu,setCheckBox,
                     isSave={info?.relation === 'me' ? true : false}
                 />
                 <div className="w-full flex-col   flex">
-                    <p className="font-semibold  text-white capitalize text-[17px]">
+                    <p className="font-semibold  dark:text-white capitalize text-[17px] text-gray-900">
                         {info?.userName}
                     </p>
-                    <p className="text-indigo-300 text-[11px] capitalize">
+                    <p className="dark:text-indigo-300 text-[11px] capitalize text-indigo-600">
                         Online
                     </p>
                 </div>
@@ -77,27 +103,45 @@ const HeaderMessage = ({ info, setShowChatInfo,showMenu,setShowMenu,setCheckBox,
                         />
                     </svg>
                 </p>
-                <p className="btn btn-ghost mask mask-squircle -mr-2" onClick={()=>setShowMenu(prev=>!prev)}>
+                <p
+                    className="btn btn-ghost mask mask-squircle -mr-2"
+                    onClick={() => setShowMenu((prev) => !prev)}
+                >
                     <BiDotsVerticalRounded size={20} color="#9ca3af" />
                 </p>
             </div>
 
             {/* menu */}
-            <div className={`menu bg-base-200  rounded-box absolute right-0 top-[70px] z-[11] w-[200px] space-y-1 transition-all duration-200  ${!showMenu?'scale-0 opacity-0 translate-x-12':'scale-100 opacity-100 translate-x-0'}`} onMouseLeave={()=>setShowMenu(false)}>
-                <div className=" select-box--item " onClick={()=>{
-                    setCheckBox(prev=>!prev)
-                    showCheckBox?setCheckMessage([]):null
-                    setShowMenu(false)
-                    }}>
+            <div
+                className={`menu bg-base-200  rounded-box absolute right-0 top-[70px] z-[11] w-[200px] space-y-1 transition-all duration-200  ${
+                    !showMenu
+                        ? 'scale-0 opacity-0 translate-x-12'
+                        : 'scale-100 opacity-100 translate-x-0'
+                }`}
+                onMouseLeave={() => setShowMenu(false)}
+            >
+                <div
+                    className=" select-box--item "
+                    onClick={() => {
+                        setCheckBox((prev) => !prev)
+                        showCheckBox ? setCheckMessage([]) : null
+                        setShowMenu(false)
+                    }}
+                >
                     <TbSquareRoundedCheck
                         size={21}
                         strokeWidth={1.5}
                         className="self-start mr-1"
                     />
 
-                    <p className={`font-[700] text-[14px] `}>{!showCheckBox?"Select Message":"Clear Selection"}</p>
+                    <p className={`font-[700] text-[14px] `}>
+                        {!showCheckBox ? 'Select Message' : 'Clear Selection'}
+                    </p>
                 </div>
-                <div className=" select-box--item " onClick={forwardContact}>
+                <div
+                    className=" select-box--item "
+                    onClick={() => forwardContact(userId)}
+                >
                     <svg
                         width={19}
                         height={19}
@@ -121,16 +165,50 @@ const HeaderMessage = ({ info, setShowChatInfo,showMenu,setShowMenu,setCheckBox,
                         />
                     </svg>
 
-                    <p className={`font-[700] text-[14px] ml-1`}>Share contact</p>
+                    <p className={`font-[700] text-[14px] ml-1`}>
+                        Share contact
+                    </p>
                 </div>
 
-                <div className=" select-box--item text-red-500 hover:bg-red-400/20" onClick={DeleteChat}>
+                <div className=" select-box--item " onClick={clearHistory}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="19"
+                        height="19"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1.5"
+                            d="M22 12c0 5.52-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2s10 4.48 10 10z"
+                        ></path>
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1.5"
+                            d="M15.71 15.18l-3.1-1.85c-.54-.32-.98-1.09-.98-1.72v-4.1"
+                        ></path>
+                    </svg>
+
+                    <p className={`font-[700] text-[14px] ml-1`}>
+                        Clear History
+                    </p>
+                </div>
+                <div
+                    className=" select-box--item text-red-500 hover:bg-red-400/20"
+                    onClick={DeleteChat}
+                >
                     <svg
                         width={18}
                         height={18}
                         viewBox="0 0 19 21"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
+                        className='text-red-500'
                     >
                         <path
                             d="M16.459 7.012c.199 0 .38.087.523.234.133.157.2.352.18.558 0 .068-.532 6.808-.837 9.645-.19 1.741-1.313 2.798-2.996 2.827-1.295.029-2.56.039-3.806.039-1.322 0-2.616-.01-3.871-.04-1.627-.038-2.75-1.114-2.932-2.826-.313-2.847-.836-9.577-.846-9.645a.79.79 0 01.19-.558.706.706 0 01.524-.234h13.87zM11.584.315c.884 0 1.674.617 1.903 1.497l.163.73a1.28 1.28 0 001.24 1.016h2.917c.389 0 .713.323.713.734v.38a.73.73 0 01-.713.734H1.233a.73.73 0 01-.713-.734v-.38c0-.411.323-.734.713-.734H4.15c.592 0 1.108-.421 1.241-1.015l.153-.682C5.78.93 6.56.315 7.455.315h4.13z"
@@ -139,7 +217,7 @@ const HeaderMessage = ({ info, setShowChatInfo,showMenu,setShowMenu,setCheckBox,
                         />
                     </svg>
 
-                    <p className={`font-[700] text-[14px] ml-1`}>Delete</p>
+                    <p className={`font-[700] text-[14px] ml-1 text-red-500`}>Delete</p>
                 </div>
             </div>
         </section>
@@ -155,7 +233,7 @@ const HeaderPinMessage = ({ pinMessage, setShowPin }) => {
                 <AiOutlineArrowLeft size={20} />
             </button>
 
-            <p className="font-semibold text-white">
+            <p className="font-semibold dark:text-white text-gray-800">
                 {pinMessage.length} Pinned Message
             </p>
         </div>
