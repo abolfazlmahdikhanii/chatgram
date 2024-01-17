e5b540a7367bc9e7784fc11a7fe385424496339f
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-17 16:34:47 +0330

    modified

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -4,16 +4,16 @@ import Profile from '../Profile/Profile'
 const UserProfile = ({userName,relation,bgProfile,profileImg,activeStatus,onForward,id}) => {
 
     return (
-        <li className='user-profile hover:bg-gray-700/20' onClick={()=>onForward(id)}>
+        <li className='user-profile dark:hover:bg-gray-700/20' onClick={()=>onForward(id)}>
            <Profile size="m" path={profileImg} userName={userName} bgProfile={bgProfile} relation={relation} isSave={relation==="me"?true:false}/>     
 
             <div className="w-full flex-col gap-0.5 flex">
                 {/* top */}
           
-                    <p className="  text-white capitalize text-[18px]">
+                    <p className="  dark:text-white capitalize text-[18px] text-gray-700 font-semibold dark:font-medium">
                         {userName}
                     </p>
-                    <p className="text-[15px] text-gray-400">
+                    <p className="text-[15px] dark:text-gray-400 text-gray-600">
                         {relation!=="me"?activeStatus:"forward here to save"}
                     </p>
                 
