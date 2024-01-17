b2d1082ff9e0d7db83a57c824c5bf33cf755a695
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-17 16:25:02 +0330

    fix bug

--- src/Components/CheckMessageBox/CheckMessageBox.jsx
+++ src/Components/CheckMessageBox/CheckMessageBox.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {AiOutlineClose} from "react-icons/ai"
 import {TiArrowForwardOutline} from "react-icons/ti"
 import {MdOutlineDeleteOutline} from "react-icons/md"
+import { ChatContext } from '../../Context/ChatContext'
 
-const CheckMessageBox = ({checkMessage,setCheckMessage,setCheck,onRemove,onForward}) => {
+const CheckMessageBox = () => {
+  const {checkMessage,setCheckMessage,removeCheckMessage,ForwardHandler,setShowCheckBox}=useContext(ChatContext)
   
   const cancelSelectHandler=()=>{
     setCheckMessage([])
@@ -14,21 +16,24 @@ const CheckMessageBox = ({checkMessage,setCheckMessage,setCheck,onRemove,onForwa
 
   {/* close */}
   <div className='flex items-center gap-1'>
-    <button className='btn btn-md btn-circle btn-ghost  grid place-items-center' onClick={()=>setCheckMessage([])}>
+    <button className='btn btn-md btn-circle btn-ghost  grid place-items-center' onClick={()=>{
+      setCheckMessage([])
+      setShowCheckBox(false)
+      }}>
         <AiOutlineClose size={21}/>
     </button>
-    <p className='text-white font-semibold '>{checkMessage.length} {checkMessage.length<=1?'Message':'Messages'}</p>
+    <p className='dark:text-white font-semibold text-gray-800'>{checkMessage.length} {checkMessage.length<=1?'Message':'Messages'}</p>
   </div>
 {/* btn */}
   <div className=' flex items-center gap-2 pr-2'>
 
 {/* forward */}
-<button className='  hover:bg-[rgba(170,170,170,0.08)]  check-message--btn text-white disabled:text-gray-500' onClick={()=>onForward(true)} disabled={checkMessage.length>0?false:true}>
+<button className=' hover:bg-[rgba(170,170,170,0.20)] dark:hover:bg-[rgba(170,170,170,0.08)] text-gray-600 check-message--btn dark:text-white disabled:text-gray-500' onClick={()=>ForwardHandler(true)} disabled={checkMessage.length>0?false:true}>
     <TiArrowForwardOutline size={26}/>
     <p className=' font-semibold capitalize text-base '>Forward</p>
 </button>
 {/* remove */}
-<button className='  hover:bg-red-500/10  check-message--btn' onClick={onRemove}>
+<button className='  hover:bg-red-500/10  check-message--btn' onClick={removeCheckMessage}>
     <MdOutlineDeleteOutline size={26} color='rgb(239,68,68)'/>
     <p className='text-red-500 font-semibold capitalize text-base'>Delete</p>
 </button>
