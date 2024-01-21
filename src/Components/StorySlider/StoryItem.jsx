f39731959a533e23b4c091c2a83374acbd6559a3
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-21 20:39:25 +0330

    make item for story

---
+++ src/Components/StorySlider/StoryItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const StoryItem = () => {
+    return (
+        <div className="flex flex-col gap-2 justify-center items-center ">
+            <div className='p-1 relative '>
+                <div className="w-[67px] h-[67px] mask mask-squircle z-[5] relative">
+                    <img src="../../../src/assets/images/profile.jpg" alt="" />
+                </div>
+              
+                {/* border */}
+                
+                <div className='w-full h-full absolute top-0 left-0  mask mask-squircle bg-base-100 z-[3]'>
+                       
+                </div>
+                <div data-color="purple" className='w-[83px] h-[82px]  absolute -top-1 -left-1  mask mask-squircle  z-[2] '>
+                       
+                </div>
+            </div>
+            <p className="text-gray-200 font-medium truncate">abolfazl</p>
+        </div>
+    )
+}
+
+export default StoryItem
