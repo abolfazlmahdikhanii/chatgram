8e74291a3b70f8fdf891793eab61b4c4097131f6
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-30 21:36:57 +0330

    modified

--- src/data.js
+++ src/data.js
@@ -9,6 +9,20 @@ const chatData=[
         profileImg: '../../../src/assets/images/profile.jpg',
         bgProfile: 'violet',
         messages: [],
+        stories:[
+            {
+                id: 1,
+                src: '../../../../src/assets/images/profile.jpg',
+            },
+            {
+                id: 2,
+                src: 'https://newspaperw-cdn.varzesh3.com/newspapers/2024/01/28/A/bmbokbmg.jpg?w=870',
+            },
+            {
+                id: 3,
+                src: 'https://newspaperw-cdn.varzesh3.com/newspapers/2024/01/28/A/cqm5akgy.jpg?w=870',
+            }
+        ]
     },
     {
         id: 2,
@@ -19,6 +33,20 @@ const chatData=[
         profileImg:
             'https://imgv3.fotor.com/images/gallery/Realistic-Male-Profile-Picture.jpg',
         messages: [],
+        stories:[
+            {
+                id: 1,
+                src: 'https://static.football360.ir/nesta/media/posts_media/photo_2024-01-30_16-56-05.jpg?x-img=v1/optimize,q_50,lossless_false,/w_328,h_240,',
+            },
+            {
+                id: 2,
+                src: 'https://newspaperw-cdn.varzesh3.com/newspapers/2024/01/28/A/bmbokbmg.jpg?w=870',
+            },
+            {
+                id: 3,
+                src: 'https://newspaperw-cdn.varzesh3.com/newspapers/2024/01/28/A/cqm5akgy.jpg?w=870',
+            }
+        ]
     },
     {
         id: 3,
@@ -29,6 +57,20 @@ const chatData=[
         profileImg:
             'https://images.nightcafe.studio/jobs/X0DIQhUI5yfPMmykyDSi/X0DIQhUI5yfPMmykyDSi--4--a0vw0.jpg?tr=w-1600,c-at_max',
         messages: [],
+        stories:[
+            {
+                id: 1,
+                src: 'https://static.football360.ir/nesta/media/posts_media/photo_2024-01-30_11-59-53.jpg?x-img=v1/optimize,q_50,lossless_false,/w_328,h_240,',
+            },
+            {
+                id: 2,
+                src: 'https://newspaperw-cdn.varzesh3.com/newspapers/2024/01/28/A/bmbokbmg.jpg?w=870',
+            },
+            {
+                id: 3,
+                src: 'https://newspaperw-cdn.varzesh3.com/newspapers/2024/01/28/A/cqm5akgy.jpg?w=870',
+            }
+        ]
     },
 ]
 
