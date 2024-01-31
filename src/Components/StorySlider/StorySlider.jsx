ca38eab1436feb4d38f8ee063534e69a400a932f
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-01-31 22:32:47 +0330

    modified

--- src/Components/StorySlider/StorySlider.jsx
+++ src/Components/StorySlider/StorySlider.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StoryItem from './StoryItem'
+import chatData from '../../data'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import StoryModal from '../UI/StoryModal/StoryModal'
 
 const StorySlider = () => {
+    const [showStoryModal,setShowStoryModal]=useState(false)
+    const [currentUserStoryIndex,setCurrentUserStoryIndex]=useState(null)
+    const storyProfileClickHandler = (id) => {
+        const findedIndex=chatData.findIndex((item)=>item.id===id)
+        setCurrentUserStoryIndex(findedIndex)
+        setShowStoryModal(true)
+    }
+  
     return (
-        <div >
+        <div>
             <div className="py-1 px-2 mb-4">
                 <h2 className="font-bold text-2xl dark:text-white text-gray-800">
                     Stories
                 </h2>
             </div>
+
             <Swiper
                 slidesPerView={3}
                 spaceBetween={10}
                 className="w-full px-2 pt-1 pb-5"
             >
-                <SwiperSlide>
-                    <StoryItem />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <StoryItem />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <StoryItem />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <StoryItem />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <StoryItem />
-                </SwiperSlide>
+                {chatData.map((user) => (
+                    <SwiperSlide key={user.id}>
+                        <StoryItem userName={user.userName} profileImg={user.profileImg} onStoryClick={()=>storyProfileClickHandler(user.id)}/>
+                    </SwiperSlide>
+                ))}
             </Swiper>
 
-            <StoryModal/>
+            {
+                showStoryModal&&currentUserStoryIndex!==null&&(
+                    <StoryModal show={showStoryModal} currentUserStory={currentUserStoryIndex} close={setShowStoryModal} />
+                )
+            }
         </div>
     )
 }
