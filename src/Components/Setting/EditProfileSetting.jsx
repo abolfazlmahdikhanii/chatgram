db80d6a47773b31bbced7a51a4b34459fec26151
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-04-06 16:46:09 +0330

    create component for profile setting

---
+++ src/Components/Setting/EditProfileSetting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SettingContainer from './SettingContainer'
+
+const EditProfileSetting = ({ close }) => {
+  return (
+    <SettingContainer title="Edit Profile" onBack={close}>
+      gggg
+    </SettingContainer>
+  )
+}
+
+export default EditProfileSetting
