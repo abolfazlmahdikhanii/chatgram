a715972d0cc81164cf6a300da6df63d55c722c44
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-04-08 01:59:11 +0330

    modified

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -18,17 +18,20 @@ const Layout = () => {
   const [showEditProfile, setShowEditProfile] = useState(false)
   const [profile, setProfile] = useState(null)
   const match = useParams()
-
+  const showSettingPanel=()=>{
+    setShowSetting(prev=>!prev)
+    setShowEditProfile(false)
+  }
   return (
     <ChatProvider>
       <div
         className={`${
           match.id
-            ? 'grid-cols-[95px_340px_1fr_280px]'
-            : 'grid-cols-[95px_340px_1fr]'
+            ? 'grid-cols-[95px_360px_1fr_280px]'
+            : 'grid-cols-[95px_360px_1fr]'
         } h-screen grid  overflow-hidden`}
       >
-        <SideMenu showSetting={setShowSetting} />
+        <SideMenu showSetting={showSettingPanel} />
         {!showSetting&&!showEditProfile && <MessageList />}
         {showSetting && <Setting profile={profile} setProfile={setProfile} setShowEditProfile={setShowEditProfile} close={()=>setShowSetting(false)} />}
         {showEditProfile && <EditProfileSetting profile={profile}  close={()=>{
