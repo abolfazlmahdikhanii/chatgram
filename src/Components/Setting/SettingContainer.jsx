023a39ad2cc64fa495000da30435303c7ed804aa
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-04-09 15:35:43 +0330

    modified

--- src/Components/Setting/SettingContainer.jsx
+++ src/Components/Setting/SettingContainer.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '../UI/Box/Box'
-import { IoMdClose } from 'react-icons/io'
-const SettingContainer = ({ title,onBack, children }) => {
+import { BiDotsVerticalRounded } from 'react-icons/bi'
+const SettingContainer = ({ title, onBack, children }) => {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <Box style={'ml-3 overflow-hidden'}>
       {/* header */}
@@ -42,10 +43,38 @@ const SettingContainer = ({ title,onBack, children }) => {
         </div>
         <button
           className="btn btn-ghost mask mask-squircle btn-sm"
-          // onClick={() => setChatInfo(false)}
+          onClick={() => setShowMenu((prev) => !prev)}
         >
-          <IoMdClose size={20} />
+          <BiDotsVerticalRounded size={20} color="#9ca3af" />
         </button>
+        {/* menu */}
+        <div
+          className={`menu bg-base-200  rounded-box absolute right-0 top-[50px] z-[11] w-[200px] space-y-1 transition-all duration-200  ${
+            !showMenu
+              ? 'scale-0 opacity-0 translate-x-12'
+              : 'scale-100 opacity-100 translate-x-0'
+          }`}
+          onMouseLeave={() => setShowMenu(false)}
+        >
+          <div className=" select-box--item ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+              />
+            </svg>
+
+            <p className={`font-[700] text-[14px] ml-1`}>Log Out</p>
+          </div>
+        </div>
       </div>
 
       {children}
