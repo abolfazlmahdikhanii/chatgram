e24e0f8612e38c9a712e760b1560a20d1a6984c5
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-04-15 13:29:08 +0330

    modified

--- src/Components/Setting/SettingItem.jsx
+++ src/Components/Setting/SettingItem.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
 
 const SettingItem = ({ icon, title, onSetting }) => {
+ 
   let ico = null
   switch (icon) {
     case 'profile':
