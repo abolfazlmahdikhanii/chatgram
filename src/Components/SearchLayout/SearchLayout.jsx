55af72d8abe42e8ff588508420723bc159e1cc8e
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-04-26 20:48:34 +0330

    show search result

--- src/Components/SearchLayout/SearchLayout.jsx
+++ src/Components/SearchLayout/SearchLayout.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ColumnProfile from '../ColumnProfile/ColumnProfile'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
+import { ChatContext } from '../../Context/ChatContext'
+import MessageItem from '../Messageitem/MessageItem'
 const SearchLayout = ({ chatData }) => {
+  console.log(chatData)
+  const { searchChat } = useContext(ChatContext)
   return (
     <div className=" w-full ">
       <Swiper
@@ -11,12 +15,29 @@ const SearchLayout = ({ chatData }) => {
         className="w-full px-2 pt-1 pb-5"
       >
         {chatData?.map((profile) => (
-          <SwiperSlide key={profile.id} className=''>
+          <SwiperSlide key={profile.id} className="">
             <ColumnProfile {...profile} />
           </SwiperSlide>
         ))}
       </Swiper>
       <p className="border-[3px] border-base-200 mt-2 -mx-3"></p>
+
+      <div className='my-3'>
+        {searchChat&&chatData
+          .filter(
+            (item, i) =>
+              item?.userName.toLowerCase().includes(searchChat.toLowerCase()) ||
+              item?.messages[i]?.messageDis.includes(searchChat)
+          )
+          .map((data) => (
+            <MessageItem
+              key={data.id}
+              {...data}
+              isSave={data.relation === 'me' ? true : false}
+              // onContext={(e) => contextMenuHandler(e, data.id)}
+            />
+          ))}
+      </div>
     </div>
   )
 }
