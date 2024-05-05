3cc03df32869406612914eed83faadacc2565cdd
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-05-05 17:57:54 +0330

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from './Components/Layout/Layout'
+import useChangeThem from './CustomHooks/useChangeThem'
 
 const App = () => {
+  const [them,setThem]=useChangeThem('light')
+  useEffect(()=>{
+   setThem(localStorage.getItem('them'))
+  },[])
   return (
     <div>
       <Layout/>
