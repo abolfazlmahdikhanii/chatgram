324c2b4e070e5401a61a898716e76ea0aeadff8e
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-05-05 17:58:17 +0330

    modified

--- src/Components/StoryImage/StoryImage.jsx
+++ src/Components/StoryImage/StoryImage.jsx
@@ -11,7 +11,7 @@ const StoryImage = ({ src }) => {
         className={`w-full h-auto object-cover aspect-video`}
       />
       {!storyLoading && (
-        <span className="loading loading-spinner loading-md"></span>
+        <span className="loading loading-spinner loading-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></span>
       )}
     </>
   )
