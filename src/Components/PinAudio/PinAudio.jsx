69c19356d619a13bce4694d6cc83b800144cc926
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-06-29 22:11:12 +0330

    modified

--- src/Components/PinAudio/PinAudio.jsx
+++ src/Components/PinAudio/PinAudio.jsx
@@ -5,90 +5,77 @@ import { FaForward } from 'react-icons/fa6'
 import { FaPause } from 'react-icons/fa'
 import { IoClose } from 'react-icons/io5'
 import { MusicControlContext } from '../../Context/MusicContext'
+import { ChatContext } from '../../Context/ChatContext'
 
-const PinAudio =() => {
-    const [check, setCheck] = useState(false)
-    const {
-        isPlay,
-        currentSong,
-        currentTimeMusic,
-        playMusic,
-        pauseMusic,
-        seekMusic,
-        durationMusic,
-        containerBox,
-        playbackRateMusic,
+const PinAudio = () => {
+  const [check, setCheck] = useState(false)
+  const {
+    isPlay,
+    currentSong,
+    currentTimeMusic,
+    playMusic,
+    pauseMusic,
+    seekMusic,
+    durationMusic,
+    containerBox,
+    playbackRateMusic,
+  } = useContext(MusicControlContext)
 
-    } = useContext(MusicControlContext)
+  const { setAudio,audio } = useContext(ChatContext)
 
- 
+  // useEffect(()=>{
+  //     playMusic(currentSong, containerBox)
+  // },[])
 
- 
+  const togglePlay = () => {
+    playMusic(currentSong, containerBox)
+  }
 
- 
+  const handlePlayBackRate = (e) => {
+    e.target.checked ? playbackRateMusic(2, true) : playbackRateMusic(1, false)
+  }
 
-
-    const togglePlay = () => {
-        playMusic(currentSong, containerBox)
-    }
-
-    const handlePlayBackRate = (e) => {
-        e.target.checked
-            ? playbackRateMusic(2, true)
-            : playbackRateMusic(1, false)
-    }
-
-    return (
-        <div className="py-2 px-4 bg-base-200  flex items-center  justify-between sticky top-0 relative">
-            <div className="flex items-center gap-2  justify-between w-full">
-                <div className="flex items-center gap-x-0.5">
-                    
-                    <button
-                        className="btn text-indigo-600 "
-                        onClick={togglePlay}
-                    >
-                        {isPlay ? <FaPlay size={18} /> : <FaPause size={18} />}
-                    </button>
-             
-                </div>
-                <div className="flex items-center gap-0.5">
-                    <label htmlFor="chk" className=" btn text-lg relative ">
-                        <input
-                            type="checkbox"
-                            className="absolute top-0 opacity-0"
-                            id="chk"
-                            onChange={(e) => {
-                                setCheck((prev) => !prev)
-                                handlePlayBackRate(e)
-                            }}
-                        />
-                        <span
-                            className={`font-sans ${
-                                check ? 'text-indigo-500' : ''
-                            }`}
-                        >
-                            2X
-                        </span>
-                    </label>
-                    <button className="btn">
-                        <IoClose size={21} />
-                    </button>
-                </div>
-            </div>
-            <div
-                className="absolute -bottom-0 left-0 w-full cursor-pointer"
-            
-            >
-                <div
-                    // onClick={handleProgressBarClick}
-                    className="h-1  hover:h-1.5  bg-indigo-600 w-0 block cursor-pointer"
-                    style={{width:(currentTimeMusic / durationMusic) * 100+"%" }}
-                    
-                ></div>
-            </div>
-
+  return (
+    <div
+      className={`py-2 px-4 bg-base-200   items-center  justify-between sticky top-0  ${
+        audio ? 'flex' : 'hidden'
+      }`}
+    >
+      <div className="flex items-center gap-2  justify-between w-full">
+        <div className="flex items-center gap-x-0.5">
+          <button className="btn text-indigo-600 " onClick={togglePlay}>
+            {isPlay ? <FaPlay size={18} /> : <FaPause size={18} />}
+          </button>
         </div>
-    )
+        <div className="flex items-center gap-0.5">
+          <label htmlFor="chk" className=" btn text-lg relative ">
+            <input
+              type="checkbox"
+              className="absolute top-0 opacity-0"
+              id="chk"
+              onChange={(e) => {
+                setCheck((prev) => !prev)
+                handlePlayBackRate(e)
+              }}
+            />
+            <span className={`font-sans ${check ? 'text-indigo-500' : ''}`}>
+              2X
+            </span>
+          </label>
+          <button className="btn" onClick={() => setAudio(null)}>
+            <IoClose size={21} />
+          </button>
+        </div>
+      </div>
+      <div className="absolute -bottom-0 left-0 w-full cursor-pointer">
+        <div
+          // onClick={handleProgressBarClick}
+          className="h-1  hover:h-1.5  bg-indigo-600 w-0 block cursor-pointer"
+          style={{ width: (currentTimeMusic / durationMusic) * 100 + '%' }}
+        ></div>
+      </div>
+    </div>
+  )
 }
 
 export default PinAudio
