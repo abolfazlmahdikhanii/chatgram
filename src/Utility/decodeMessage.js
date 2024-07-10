72cc84231aae72b2e6a26c837ad14225488010a1
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-07-10 17:40:14 +0330

    decode encrypte message

---
+++ src/Utility/decodeMessage.js
@@ -0,0 +1,15 @@
+
+const decodeMessage = (content) => {
+  // Extract hexadecimal values using regular expression
+  // Remove the leading '\\x' and split the string into pairs of hexadecimal values
+  const hexPairs = content.slice(2).match(/.{1,2}/g) || []
+
+  // Convert each pair of hexadecimal values to characters and join them
+  const text = hexPairs
+    .map((hex) => String.fromCharCode(parseInt(hex, 16)))
+    .join('')
+
+  
+  return text
+}
+export default decodeMessage
