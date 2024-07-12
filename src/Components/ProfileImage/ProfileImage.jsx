087614baf3cde62c905c491bd1ab42d4dfb033b1
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-07-12 22:21:25 +0330

    modified

--- src/Components/ProfileImage/ProfileImage.jsx
+++ src/Components/ProfileImage/ProfileImage.jsx
@@ -2,46 +2,49 @@ import React, { useEffect, useState } from 'react'
 import { BsFillBookmarkFill } from 'react-icons/bs'
 import userNameSpliter from '../../Utility/userNameSpliter'
 const ProfileImage = ({
-    size,
-    src,
-    userName,
-    bgProfile,
-    relation,
-    isSave = false,
+  size,
+  src,
+  userName,
+  email,
+  bgProfile,
+  relation,
+  isSave = false,
 }) => {
-    let sizes = 16
-    if (size === 'm') sizes = 22
-    if (size === 'lg') sizes = 48
+  let sizes = 16
+  if (size === 'm') sizes = 22
+  if (size === 'lg') sizes = 48
 
-  
-    return (
-        <div className="avatar">
-            <div
-                className={`mask mask-squircle w-11 h-11 ${
-                    size === 'm' && 'w-14 h-14'
-                } ${size === 'xs' && 'w-5 h-5'} ${
-                    size === 'lg' && 'w-32 h-32'
-                }`}
-            >
-                {src && !isSave ? (
-                    <img src={src} alt="profile" className="w-full h-full" />
-                ) : (
-                    <div
-                        data-color={bgProfile}
-                        className={`grid place-items-center text-white h-full font-bold w-full  `}
-                    >
-                        {!src || !isSave ? (
-                            <span>{userName && userNameSpliter(userName)}</span>
-                        ) : (
-                            <span>
-                                <BsFillBookmarkFill size={sizes} color="#fff" />
-                            </span>
-                        )}
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+  return (
+    <div className="avatar">
+      <div
+        className={`mask mask-squircle w-11 h-11 ${
+          size === 'm' && 'w-14 h-14 text-lg'
+        } ${size === 'xs' && 'w-5 h-5'} ${size === 'lg' && 'w-32 h-32 text-4xl'}`}
+      >
+        {src && !isSave ? (
+          <img src={src} alt="profile" className="w-full h-full" />
+        ) : (
+          <div
+            data-color={bgProfile}
+            className={`grid place-items-center text-white h-full font-bold w-full  `}
+          >
+            {!src && !isSave && (
+              <span>
+                {userName
+                  && userNameSpliter(userName)
+                }
+              </span>
+            )}
+            {!src && isSave && (
+              <span>
+                <BsFillBookmarkFill size={sizes} color="#fff" />
+              </span>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default ProfileImage
