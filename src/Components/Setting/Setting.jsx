6a44c66256152fdcd2e2065cae557f1099e3f882
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-07-13 19:47:45 +0330

    save setting in database

--- src/Components/Setting/Setting.jsx
+++ src/Components/Setting/Setting.jsx
@@ -1,35 +1,31 @@
 import React, { useContext, useEffect } from 'react'
 import { IoMdClose } from 'react-icons/io'
-import { ChatContext } from '../../Context/ChatContext'
 import Profile from '../Profile/Profile'
 import { useState } from 'react'
 import SettingItem from './SettingItem'
 import SettingContainer from './SettingContainer'
+import { UserContext } from '../../Context/UserContext'
 const Setting = ({setShowEditProfile,close,profile,setProfile, setShowGeneralSetting}) => {
 
   
-  const { chat, message } = useContext(ChatContext)
-  useEffect(() => {
-    setProfile(chat.find((item) => item.relation === 'me'))
-  
-  }, [])
+  const { user } = useContext(UserContext)
+
   return (
     <>
       <SettingContainer title="Setting" onBack={close}>
         <div className="mt-12 flex flex-col gap-5 items-center justify-center border-b-[12px] pb-7 border-base-300 -mx-2">
           <div className=" flex items-center justify-center">
             <Profile
-              path={profile?.profileImg}
-              userName={profile?.userName}
-              bgProfile={profile?.bgProfile}
-              relation={profile?.relation}
+              path={user?.avatar_url}
+              userName={user?.username||user.email.split('@')[0]}
+              bgProfile={user?.bgProfile}
               isSave={false}
               size="lg"
             />
           </div>
           <div className="space-y-1.5">
-            <p className="text-3xl text-gray-900 dark:text-gray-50 font-semibold truncate">
-              {profile?.userName}
+            <p className="text-2xl text-gray-900 dark:text-gray-50 font-semibold truncate">
+              {user?.username||user.email.split('@')[0]}
             </p>
             <p className="text-center text-sm text-indigo-400">Online</p>
           </div>
