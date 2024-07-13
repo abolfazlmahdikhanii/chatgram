fe1c10e1adc6f66a5e4202433f342513c60cf518
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-07-13 19:48:11 +0330

    save user login info

---
+++ src/Context/UserContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from "react";
+
+export const UserContext=createContext()
+
+export const UserProvider=({children})=>{
+    const [user,setUser]=useState(null)
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('profile');
+        if (storedUser) {
+          setUser(JSON.parse(storedUser));
+        }
+      }, []);
+    
+      return (
+        <UserContext.Provider value={{ user, setUser }}>
+          {children}
+        </UserContext.Provider>
+      );
+}
