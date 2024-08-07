cf681566b887d9a9090102dc77de7e17c3d45e2b
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-08-07 17:28:57 +0330

    modified

--- src/Components/MessageList/MessageList.jsx
+++ src/Components/MessageList/MessageList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Box from '../UI/Box/Box'
 import MessageItem from '../Messageitem/MessageItem'
 import { ChatContext } from '../../Context/ChatContext'
@@ -8,13 +8,17 @@ import StorySlider from '../StorySlider/StorySlider'
 import SearchBar from '../UI/SearchBar/SearchBar'
 import ColumnProfile from '../ColumnProfile/ColumnProfile'
 import SearchLayout from '../SearchLayout/SearchLayout'
+import { UserContext } from '../../Context/UserContext'
+import { supabase } from '../../superbase'
 
 const MessageList = () => {
+  const { user } = useContext(UserContext)
   const [chatId, setChatId] = useState(null)
   const [pageX, setPageX] = useState(null)
   const [pageY, setPageY] = useState(null)
   const [showMenu, setShowMenu] = useState(false)
-  const { chat, DeleteChat } = useContext(ChatContext)
+  const [friends, setFriends] = useState([])
+  const { chat, DeleteChat,setChatInfo,lastMessage } = useContext(ChatContext)
   const [isActiveSearch, setIsActiveSearch] = useState(false)
   const navigate = useNavigate()
 
@@ -25,7 +29,29 @@ const MessageList = () => {
     setPageY(e.pageY)
     setChatId(id)
   }
+  useEffect(() => {
+    getFriendsList()
+    
+  }, [])
 
+  const getFriendsList = async () => {
+    try {
+      let { data: friendrequests, error } = await supabase
+        .from('friendrequests')
+        .select('requestid,senderid(*),recipientid(*)')
+        .or(`senderid.eq.${user?.userid},recipientid.eq.${user?.userid}`)
+        .eq('status','accepted')
+      
+
+      if (error) throw error
+
+      setFriends(friendrequests)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   return (
     <Box style={'ml-3 overflow-hidden'}>
       <SearchBar
@@ -41,14 +67,22 @@ const MessageList = () => {
             </h2>
           </div>
           <div className="w-full overflow-hidden space-y-2 h-screen overflow-y-auto ">
-            {chat?.map((chat) => (
+            <MessageItem
+              key={user?.userid}
+              chats={user}
+              chatID={user?.userid}
+              isSave={true}
+              onContext={(e) => contextMenuHandler(e, user?.id)}
+            />
+            {friends.length?friends?.map((chat) => (
               <MessageItem
-                key={chat.id}
-                {...chat}
-                isSave={chat.relation === 'me' ? true : false}
-                onContext={(e) => contextMenuHandler(e, chat.id)}
+                key={chat?.id}
+                chats={chat?.senderid?.userid==user?.userid?{...chat?.recipientid}:{...chat?.senderid}}
+                chatID={chat?.requestid}
+                isSave={false}
+                onContext={(e) => contextMenuHandler(e, chat?.id)}
               />
-            ))}
+            )):null}
           </div>
           <UserMenu
             show={showMenu}
