bbf941442c9d0c10c452670b4a93d6cd852a74aa
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-08-22 15:04:17 +0330

    modified

---
+++ src/Utility/messageTypeChecker.jsx
@@ -0,0 +1,14 @@
+const messageTypeChecker=(type,name)=>{
+    if (type==="mp3"&&name==="") {
+       return (<p className='text-[14px] '>Voice Message</p> )
+    }
+    if(type==="img"){
+        return (<p className='text-[14px] '>Photo</p> )
+    }
+    if(type==="video"){
+        return (<p className='text-[14px] '>Video</p> )
+    }
+}
+
+
+export default messageTypeChecker
