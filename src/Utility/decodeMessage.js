f7eebd0eadf2662cf2ee165f296e14587f90df0c
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-05 15:42:10 +0330

    modified

--- src/Utility/decodeMessage.js
+++ src/Utility/decodeMessage.js
@@ -2,7 +2,7 @@ const decodeMessage = (content,src) => {
 
   // Extract hexadecimal values using regular expression
   // Remove the leading '\\x' and split the string into pairs of hexadecimal values
-  const hexPairs = content.slice(2).match(/.{1,2}/g) || []
+  const hexPairs = content?.slice(2).match(/.{1,2}/g) || []
 
   // Convert each pair of hexadecimal values to characters and join them
   const text = hexPairs
