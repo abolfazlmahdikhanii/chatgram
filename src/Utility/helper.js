7c87df7b667c81d6b68fbe51a72aa725e22c8cec
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-07 21:48:07 +0330

    modified

--- src/Utility/helper.js
+++ src/Utility/helper.js
@@ -1,4 +1,14 @@
-export function strToByte(str) {
-    return '\\x' + Array.from(str).map(char => char.charCodeAt(0).toString(16).padStart(2, '0')).join('');
 
-  }
+
+export  function strToByte(str) {
+
+  
+    return (
+      '\\x' +
+      Array.from(str)
+        .map((char) => char.charCodeAt(0).toString(16).padStart(2, '0'))
+        .join('')
+    )
+   
+  
+}
