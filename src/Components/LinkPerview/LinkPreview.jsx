4a16b7a7f673b5f496b7781dd0f988d09c0d6051
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-12 22:17:59 +0330

    modified

--- src/Components/LinkPerview/LinkPreview.jsx
+++ src/Components/LinkPerview/LinkPreview.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import Microlink from '@microlink/react'
 
-const LinkPreview = ({text}) => {
+const LinkPreview = ({ text }) => {
   // const splitURl=url.split(' ')
-  const urls = text.match(/(https?:\/\/[^\s]+)|(www\.[^\s]+)/g);
+  const urls = text.match(/(https?:\/\/[^\s]+)|(www\.[^\s]+)/g)
 
   const renderTextWithUrls = () => {
-    let newText = text;
-    if (urls&&!text.startsWith('<img src=')) {
+    let newText = text
+    if (urls && !text?.includes('<img src=')) {
       urls.forEach((url) => {
-        newText = newText.replace(url, `<a href="${url}" target="_blank" class="underline " rel='noreferrer noopener'>${url}</a>`);
-      });
+        newText = newText.replace(
+          url,
+          `<a href="${url}" target="_blank" class="underline text-sm" rel='noreferrer noopener'>${url}</a>`
+        )
+      })
+      return newText
+    } else {
+      return text
     }
- 
-    return newText;
-      
   }
 
-
   return (
- 
-        <div dangerouslySetInnerHTML={{__html:renderTextWithUrls()}}></div>
-  
-  ) 
+    <>
+      <div dangerouslySetInnerHTML={{ __html: renderTextWithUrls() }}></div>
+      {!text?.includes('<img src=')&&<Microlink url={urls} lazy={{ threshold: 0.5 }} className="min-w-[380px] h-[150px] mt-2 rounded-lg bg-transparent" dir="auto" />}
+    </>
+  )
 }
 
 export default LinkPreview
