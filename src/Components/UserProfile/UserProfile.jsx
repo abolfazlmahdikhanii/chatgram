9dc04e02bafe017b3db418b805fb5273ecab1d8a
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-12 22:19:16 +0330

    modified

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -9,20 +9,27 @@ const UserProfile = ({
   activeStatus,
   onForward,
   saveChat,
-  fromChatID
-  
+  fromChatID,
+  forwardContact,
 }) => {
-  
-    const {user}=useContext(UserContext)
-    
-    console.log(chatID);
-    let profile=null
-    if(chats)profile=chats
-    if(saveChat)profile=saveChat
+  const { user } = useContext(UserContext)
+
+  console.log(chatID)
+  let profile = null
+  if (chats) profile = chats
+  if (saveChat) profile = saveChat
   return (
     <li
       className="user-profile dark:hover:bg-gray-700/20"
-      onClick={() => {onForward(chatID,user?.userid,profile.userid,'normal',fromChatID)}}
+      onClick={() => {
+        onForward(
+          chatID,
+          user?.userid,
+          profile.userid,
+          !forwardContact ? 'normal' : 'contact',
+          fromChatID
+        )
+      }}
     >
       <Profile
         size="m"
@@ -36,10 +43,12 @@ const UserProfile = ({
         {/* top */}
 
         <p className="  dark:text-white capitalize text-[18px] text-gray-700 font-semibold dark:font-medium">
-        {profile?.username || profile?.email?.split('@')[0]}
+          {profile?.username || profile?.email?.split('@')[0]}
         </p>
         <p className="text-[15px] dark:text-gray-400 text-gray-600">
-          {user?.userid === profile?.userid ? activeStatus : 'forward here to save'}
+          {user?.userid === profile?.userid
+            ? activeStatus
+            : 'forward here to save'}
         </p>
       </div>
     </li>
