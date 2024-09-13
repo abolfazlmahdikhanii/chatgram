5bf3b9108ad8f3c24d43339087243357810829b5
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-13 15:55:03 +0330

    modified

--- src/Components/UI/ReactionBox/ReactionBox.jsx
+++ src/Components/UI/ReactionBox/ReactionBox.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import Profile from '../../Profile/Profile'
 
-const ReactionBox = ({reaction,setReaction}) => {
-    return (
-        <div className='bg-gray-300/30  px-2 py-1  rounded-lg flex items-center justify-between my-2.5 w-[70px] ml-auto backdrop-blur-md cursor-pointer' onClick={()=>setReaction(false)}>
-            <div>
-            <Profile path={reaction?.profile.profileImg}  userName={reaction?.profile.userName} bgProfile={reaction?.profile.bgProfile} relation={reaction?.profile.relation} size="xs"/>
-            </div>
-            <div className=''>
-                <img
-                    src={`https://fonts.gstatic.com/s/e/notoemoji/latest/${reaction.reaction}/512.webp`}
-                    width="20"
-                    height="20"
-                />
-            </div>
-        </div>
-    )
+const ReactionBox = ({ reaction, setReaction }) => {
+  return (
+    <div
+      className=" bg-gray-300/30  px-2 py-1  rounded-lg flex items-center justify-between my-2.5 w-[65px] ml-auto backdrop-blur-md cursor-pointer border-2 border-indigo-700"
+      onClick={() => setReaction(false)}
+    >
+      <div>
+        <Profile
+          size="xs"
+          path={reaction?.userInfos?.avatar_url}
+          userName={
+            reaction?.userInfos?.username ||
+            reaction?.userInfos?.email?.split('@')[0]
+          }
+          bgProfile={reaction?.userInfos?.bgProfile}
+        />
+      </div>
+      <div className="">
+        <img
+          src={`https://fonts.gstatic.com/s/e/notoemoji/latest/${reaction?.emojiId}/512.webp`}
+          width="20"
+          height="20"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default ReactionBox
