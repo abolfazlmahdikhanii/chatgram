79a7da2ff08b7e9053ea7e302ef975f71c130cf2
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-17 21:13:49 +0330

    modified

--- src/Components/UI/Box/Box.jsx
+++ src/Components/UI/Box/Box.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Box = ({ children, style,context }) => {
+const Box = ({ children, style,context,mouseOut }) => {
 
     return (
         <div
             className={`bg-base-100 px-2 py-4 flex flex-col ${style}  transition-all duration-200 `}
             onContextMenu={context}
+            onMouseLeave={mouseOut}
+            
         >
             {children}
         </div>
