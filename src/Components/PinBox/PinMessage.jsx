1affcbef5787577476f43093869aa0396627782d
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-17 21:14:00 +0330

    modified

--- src/Components/PinBox/PinMessage.jsx
+++ src/Components/PinBox/PinMessage.jsx
@@ -1,40 +1,38 @@
 import React from 'react'
 import { HashLink } from 'react-router-hash-link'
 import TypeMessage from '../TypeMessage/TypeMessage'
-import messageType from '../../Utility/MessageType'
+import messageType from '../../Utility/messageTypeChecker'
+import decodeMessage from '../../Utility/decodeMessage'
 
-const PinMessage = ({ title, index, show, arr, id }) => {
-    return (
-        <>
-            <HashLink
-                to={`#${id}`}
-                scroll={(el) =>
-                    el.scrollIntoView({ behavior: 'smooth', block: 'end' })
-                }
-                className="flex gap-3"
-            >
-         
-                    {title&& title[0]?.type === 'img' || title&& title[0]?.type === 'video' ? (
-                            <TypeMessage dis={title} w={'w-9 aspect-square'} />
-                        ):null}
-              
-                <div className="flex flex-col gap-0.5 ">
-                    <p className="font-semibold text-indigo-500 text-sm">
-                        Pinned message #{index + 1}
-                    </p>
-                    <p className="text-[14px] truncate " dir='auto'>
-                        {title &&
-                        title[0]?.type !== 'img' &&
-                        title[0]?.type !== 'video' ? (
-                            <TypeMessage dis={title} />
-                        ) : (
-                           title&& messageType(title[0]?.type, title[0]?.name)
-                        )}
-                    </p>
-                </div>
-            </HashLink>
-        </>
-    )
+const PinMessage = ({ title, index, show, arr, id,type,name }) => {
+  
+  return (
+    <>
+      <HashLink
+        to={`#${id}`}
+        scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
+        className="flex gap-3"
+      >
+        {(type === 'img') ||
+        (type === 'video') ? (
+          <TypeMessage dis={title} w={'w-9 aspect-square'} />
+        ) : null}
+
+        <div className="flex flex-col gap-0.5 ">
+          <p className="font-semibold text-indigo-500 text-sm">
+            Pinned message #{index + 1}
+          </p>
+          <p className="text-[14px] truncate " dir="auto">
+            {type==='text'&&!name ? (
+              <TypeMessage dis={title} type={type} />
+            ) : (
+              messageType(type,name)
+            )}
+          </p>
+        </div>
+      </HashLink>
+    </>
+  )
 }
 
 export default PinMessage
