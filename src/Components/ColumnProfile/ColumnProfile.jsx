cb5b632837386e1f7f79b00d5f64bbcdbb4d0b22
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-18 16:16:10 +0330

    modified

--- src/Components/ColumnProfile/ColumnProfile.jsx
+++ src/Components/ColumnProfile/ColumnProfile.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import userNameSpliter from '../../Utility/userNameSpliter'
-const ColumnProfile = ({ id, userName, profileImg, bgProfile, relation }) => {
+import Profile from '../Profile/Profile'
+import { UserContext } from '../../Context/UserContext'
+import { ChatContext } from '../../Context/ChatContext'
+const ColumnProfile = ({ id, chatID, chats, activeStatus, saveChat,setActiveSearch }) => {
+  const {user}=useContext(UserContext)
+  const {setFriendID}=useContext(ChatContext)
+  let profile = null
+  if (chats) profile = chats
+  if (saveChat) profile = saveChat
+
   
   return (
     <Link
       className="flex flex-col gap-2 justify-center items-center transition-all duration-300 hover:bg-gray-600/20 px-12 py-3 w-full rounded-2xl"
-      to={`/chat/${id}`}
+      to={`/chat/${chatID}`}
+      onClick={() => {
+        setFriendID(profile?.userid)
+        setActiveSearch(false)
+      }}
     >
-      <div className="w-[65px] h-[65px] mask mask-squircle z-[5] relative">
-        {profileImg ? (
-          <img src={profileImg} alt="story img" />
-        ) : (
-          <div
-            data-color={bgProfile}
-            className={`grid place-items-center text-white h-full font-bold w-full  `}
-          >
-            {!profileImg && (
-              <span>{userName && userNameSpliter(userName)}</span>
-            )}
-          </div>
-        )}
-      </div>
-      <p className="dark:text-gray-300 font-normal truncate text-gray-700 text-sm">
-        {userName}
+    
+        <Profile
+        size="m"
+        path={profile?.avatar_url}
+        userName={profile?.username || profile?.email?.split('@')[0]}
+        bgProfile={profile?.bgProfile}
+        isSave={saveChat}
+      />
+      <p className="dark:text-gray-300 font-normal truncate text-gray-700 text-sm w-[70px]">
+      {profile?.username || profile?.email?.split('@')[0]}
       </p>
     </Link>
   )
