0c824ea8cb9a49a2384cd580c25a25e7b2d0b252
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-20 19:04:27 +0330

    modified

--- src/Components/UI/ReactionBox/ReactionBox.jsx
+++ src/Components/UI/ReactionBox/ReactionBox.jsx
@@ -4,7 +4,7 @@ import Profile from '../../Profile/Profile'
 const ReactionBox = ({ reaction, setReaction }) => {
   return (
     <div
-      className=" bg-gray-300/30  px-2 py-1  rounded-lg flex items-center justify-between my-2.5 w-[65px] ml-auto backdrop-blur-md cursor-pointer border-2 border-indigo-700"
+      className=" bg-gray-300/30  px-2 py-1 mx-1.5  rounded-lg flex items-center justify-between my-2.5 w-[65px] ml-auto backdrop-blur-md cursor-pointer border-2 border-gray-500"
       onClick={() => setReaction(false)}
     >
       <div>
