6df8a1b9c1e583eba001268cc6aabd3400d4d0b3
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-20 19:04:05 +0330

    modified

--- src/Components/FooterMessage/FooterMessage.jsx
+++ src/Components/FooterMessage/FooterMessage.jsx
@@ -26,8 +26,8 @@ const FooterMessage = ({message,date,status,edited,pin,reaction,messageType,capt
 
   return (
     <>
-      {messageType === "file"||messageType === "mp3"|| messageType == 'audio/webm' || messageType === "text"||reaction||caption ? (
-        <div className=" flex items-center gap-1 mt-1 px-3 justify-end chat-footer -mr-4">
+      {messageType === "file"||messageType?.includes('audio')||messageType === "mp3" || messageType === "text"||reaction||caption ? (
+        <div className={` flex items-center gap-1 mt-1 px-3 justify-end chat-footer -mr-4 ${messageType?.includes('img')||messageType?.includes('video')?'w-full':''}`}>
           <p className="dark:text-gray-500 text-gray-400 text-[12px] mr-1.5">{pin?<BsFillPinAngleFill size={12}/>:''}</p>
           <p className="dark:text-gray-400 text-gray-300 text-[12px] mr-0.5 italic">{edited?'edited':''}</p>
           <p className="dark:text-gray-400 text-gray-300 text-[10px]">{date}</p>
