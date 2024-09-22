91683c1f6eaa4c4458beea7f85fd3a50655e7ff6
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-22 20:41:11 +0330

    modified

--- src/Components/Setting/GeneralSetting.jsx
+++ src/Components/Setting/GeneralSetting.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import SettingContainer from './SettingContainer'
-import { ChatContext } from '../../Context/ChatContext'
+import { supabase } from '../../superbase'
+import { UserContext } from '../../Context/UserContext'
 
 const GeneralSetting = ({ close }) => {
   const images = [
@@ -81,17 +82,52 @@ const GeneralSetting = ({ close }) => {
       src: '../../../src/assets/images/chat-bg/pattern-9.svg',
     },
   ]
-  const { font, setFont, setChatBg, chatBg } = useContext(ChatContext)
 
-  const isSelectdImage=(img)=>{
-    const splitImg=img.split('/')
-    const splitbg=chatBg.split('/')
+  const {user,font, setFont, setChatBg, chatBg}=useContext(UserContext)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isChange, setIsChange] = useState(false)
+  const saveToStorage=(bg,font)=>{
+    const getStroage=localStorage.getItem('profile')?JSON.parse(localStorage.getItem('profile')):[]
 
-    return  splitImg[splitImg.length-1]==splitbg[splitbg.length-1]
+    getStroage.chatBg=bg
+    getStroage.font=font
+    // Object.assign(getStroage,{chatBg:bg,fontSize:font})
+
+    localStorage.setItem('profile',JSON.stringify(getStroage))
+    // {getStroage,chatBg:bg,font:font}
+  }
+  const isSelectdImage = (img) => {
+    const splitImg = img.split('/')
+    const splitbg = chatBg
+
+    return splitImg[splitImg.length - 1] == splitbg
+  }
+
+  const updateSettingHandler = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    try {
+      const splitbg = chatBg?.split('/')
+      const { data, error } = await supabase
+        .from('user_setting')
+        .upsert({id:user?.userid,chatBg:splitbg[splitbg?.length - 1],fontSize:font},{onConflict:'id'})
+        .select()
+      if (error) throw error
+      // setUser(data[0])
+      console.log(data)
+      saveToStorage(data[0].chatBg,data[0].font)
+      setChatBg(data[0].chatBg)
+      setFont(data[0].fontSize)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+    // close edit profile
   }
   return (
     <SettingContainer title="General" onBack={close}>
-      <div className="px-4 py-2">
+      <div className="px-4 py-2 relative">
         <p className="text-[#8774E1] text-[17px] font-semibold">Setting</p>
         <div className="px-1 py-6">
           <div className="flex items-center justify-between">
@@ -105,7 +141,10 @@ const GeneralSetting = ({ close }) => {
             max="22"
             value={font}
             className="range range-xs my-5 range-primary"
-            onChange={(e) => setFont(e.target.value)}
+            onChange={(e) => {
+              setFont(e.target.value)
+              setIsChange(true)
+            }}
           />
         </div>
         <p className="border-[7px] border-base-200 w-[150%] -mx-[30px]"></p>
@@ -113,9 +152,9 @@ const GeneralSetting = ({ close }) => {
           Chat Wallpaper
         </p>
         <div className="grid grid-cols-3 gap-x-3 my-2 gap-y-4 overflow-y-auto h-[360px] n-scroll scroll-smooth">
-          {images.map((item,i) => (
+          {images.map((item, i) => (
             <label
-              key={i+1}
+              key={i + 1}
               className={`w-full h-[125px] transition-all duration-200 cursor-pointer  rounded-lg overflow-hidden `}
             >
               <input type="radio" name="bg-check" className="peer hidden" />
@@ -126,11 +165,41 @@ const GeneralSetting = ({ close }) => {
                 } `}
                 defaultChecked
                 alt=""
-                onClick={(e) => setChatBg(e.target.src)}
+                onClick={(e) => {
+                  const splited=e.target.src?.split('/')
+                  setChatBg(splited[splited?.length-1])
+                  setIsChange(true)
+                }}
               />
             </label>
           ))}
         </div>
+        {isChange ? (
+          <button
+            className="btn btn-success mask mask-squircle fixed bottom-7 ml-[245px] text-white "
+            onClick={updateSettingHandler}
+            disabled={isLoading}
+          >
+            {!isLoading ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+            ) : (
+              <span className="loading loading-spinner w-[2rem] absolute right-4"></span>
+            )}
+          </button>
+        ):null}
       </div>
     </SettingContainer>
   )
