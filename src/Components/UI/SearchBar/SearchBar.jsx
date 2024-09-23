b4c714bdfaa022d73b05d018ebf19c01f8c3931e
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-23 17:36:41 +0330

    modified

--- src/Components/UI/SearchBar/SearchBar.jsx
+++ src/Components/UI/SearchBar/SearchBar.jsx
@@ -3,19 +3,41 @@ import { ChatContext } from '../../../Context/ChatContext'
 import { supabase } from '../../../superbase'
 
 const SearchBar = ({ activeSearch, setActiveSearch }) => {
-  const { setSearchChat, searchChat } = useContext(ChatContext)
+  const { setSearchChat, searchChat,setSearchText,setSearchLoading } = useContext(ChatContext)
   const [search,setSearch]=useState('')
   const searchFriendHandler = async (e) => {
     try {
       const val=e.target.value.trim()
+      setSearchLoading(true)
       let { data: users, error } = await supabase
       .from('users')
       .select('*')
       .or(`username.ilike.%${val}%,email.ilike.%${val}%`);
       if (error) throw Error
       setSearchChat(users)
+      setSearchLoading(false)
     } catch (error) {
       console.log(error)
+    }
+    finally{
+      setSearchLoading(false)
+    }
+  }
+  const showAllUserHandler = async () => {
+    try {
+      setSearchLoading(true)
+      let { data: users, error } = await supabase
+      .from('users')
+      .select('*')
+      
+      if (error) throw Error
+      setSearchChat(users)
+      setSearchLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+    finally{
+      setSearchLoading(false)
     }
   }
   return (
@@ -62,15 +84,20 @@ const SearchBar = ({ activeSearch, setActiveSearch }) => {
             ? 'w-[100%] border border-primary'
             : 'w-full border-transparent'
         }`}
-        onClick={() => setActiveSearch(true)}
+        onClick={() => {
+          setActiveSearch(true)
+          showAllUserHandler()
+        }}
       >
         <input
           type="text"
           className=" py-2.5 pl-4 w-full bg-transparent focus-visible:outline-none dark:text-white dark:placeholder:text-gray-500 placeholder:text-sm text-gray-800 placeholder:text-gray-600"
           placeholder="Search..."
           value={search}
+          dir='auto'
           onChange={(e) => {
             setSearch(e.target.value.trim())
+            setSearchText(e.target.value.trim())
             searchFriendHandler(e)
           }}
         />
@@ -80,7 +107,7 @@ const SearchBar = ({ activeSearch, setActiveSearch }) => {
           height="20"
           fill="none"
           viewBox="0 0 24 24"
-          className={`text-gray-400 transition-all duration-300 ${
+          className={`text-gray-400 ml-1.5 transition-all duration-300 ${
             activeSearch ? 'text-primary' : ''
           }`}
         >
