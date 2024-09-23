d82ebc205e04af3653af5c2b5c5a3b0e37990e77
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-23 17:36:08 +0330

    modified

--- src/Components/SearchLayout/SearchLayout.jsx
+++ src/Components/SearchLayout/SearchLayout.jsx
@@ -6,8 +6,9 @@ import { ChatContext } from '../../Context/ChatContext'
 import MessageItem from '../Messageitem/MessageItem'
 import { UserContext } from '../../Context/UserContext'
 import SearchItem from '../SearchItem/SearchItem'
+import SkeletonLoader from '../UI/SkeletonLoader/SkeletonLoader'
 const SearchLayout = ({ chatData, setActiveSearch }) => {
-  const { searchChat } = useContext(ChatContext)
+  const { searchChat, searchText, searchLoading } = useContext(ChatContext)
   const { user } = useContext(UserContext)
   const forwardUserList = [{ meID: user }, ...chatData]
   const filterFriend = (id) => {
@@ -51,14 +52,40 @@ const SearchLayout = ({ chatData, setActiveSearch }) => {
       <p className="border-[3px] border-base-200 mt-2 -mx-3"></p>
 
       <div className="my-3">
-        {searchChat&&searchChat.map((chat, i, arr) => (
-          <SearchItem
-            key={chat?.id}
-            chats={chat}
-            isSave={user.userid == chat?.userid}
-            isFriend={filterFriend(chat?.userid)}
-          />
-        ))}
+        {searchLoading ? (
+          <>
+            <SkeletonLoader/>
+            <SkeletonLoader/>
+         
+          </>
+        ) : (
+          <>
+            {searchChat?.length ? (
+              searchChat.map((chat, i, arr) => (
+                <SearchItem
+                  key={chat?.id}
+                  chats={chat}
+                  isSave={user.userid == chat?.userid}
+                  isFriend={filterFriend(chat?.userid)}
+                />
+              ))
+            ) : (
+              <div className="h-full w-[65%] grid place-items-center my-[65px] mx-auto">
+                <img
+                  src="../../../src/assets/images/Search.svg"
+                  className="w-full h-full"
+                  alt=""
+                />
+                <p className="dark:text-gray-400 text-gray-500 -mt-2">
+                  No results{' '}
+                </p>
+                <p className="dark:text-gray-500 text-gray-400 mt-1 text-sm truncate max-w-[250px]">
+                  There were no results for {`"${searchText}"`}{' '}
+                </p>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </div>
   )
