10e621e9e0e00e8e104fe2ed0d69620ec8a50d16
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-24 17:10:00 +0330

    add color nme

--- src/Components/Setting/GeneralSetting.jsx
+++ src/Components/Setting/GeneralSetting.jsx
@@ -79,18 +79,20 @@ const GeneralSetting = ({ close }) => {
     },
   ]
 
-  const { user, font, setFont, setChatBg, chatBg } = useContext(UserContext)
+  const { user, font, setFont, setChatBg, chatBg,color, setColor } = useContext(UserContext)
   const [isLoading, setIsLoading] = useState(false)
+
 
   const [isChange, setIsChange] = useState(false)
 
-  const saveToStorage = (bg, font) => {
+  const saveToStorage = (bg, font,color) => {
     const getStroage = localStorage.getItem('profile')
       ? JSON.parse(localStorage.getItem('profile'))
       : []
 
     getStroage.chatBg = bg
-    getStroage.font = font
+    getStroage.fontSize = font
+    getStroage.profileColor = color
     // Object.assign(getStroage,{chatBg:bg,fontSize:font})
 
     localStorage.setItem('profile', JSON.stringify(getStroage))
@@ -115,6 +117,7 @@ const GeneralSetting = ({ close }) => {
             id: user?.userid,
             chatBg: splitbg[splitbg?.length - 1],
             fontSize: font,
+            profileColor:color
           },
           { onConflict: 'id' }
         )
@@ -122,9 +125,10 @@ const GeneralSetting = ({ close }) => {
       if (error) throw error
       // setUser(data[0])
       console.log(data)
-      saveToStorage(data[0].chatBg, data[0].font)
+      saveToStorage(data[0].chatBg, data[0].fontSize,data[0].profileColor)
       setChatBg(data[0].chatBg)
       setFont(data[0].fontSize)
+      setColor(data[0].profileColor)
       setIsChange(false)
     } catch (error) {
       console.log(error)
@@ -142,6 +146,16 @@ const GeneralSetting = ({ close }) => {
           localStorage.getItem('profile')
             ? JSON.parse(localStorage.getItem('profile'))?.chatBg
             : chatBg
+        )
+        setFont(
+          localStorage.getItem('profile')
+            ? JSON.parse(localStorage.getItem('profile'))?.fontSize
+            : font
+        )
+        setColor(
+          localStorage.getItem('profile')
+            ? JSON.parse(localStorage.getItem('profile'))?.profileColor
+            : color
         )
       }}
     >
@@ -165,59 +179,114 @@ const GeneralSetting = ({ close }) => {
             }}
           />
         </div>
-        <p className="border-[7px] border-base-200 w-[150%] -mx-[30px]"></p>
+        <p className="border-[7px] border-base-200 w-[150%] -mx-[30px]" ></p>
         <p className="text-[#8774E1] text-[17px] font-semibold py-5 ">
           Name Colors
         </p>
         <div className=" gap-x-3 my-1 gap-y-4 ">
-          <div>
-            <div className="chat chat-start" >
-              <div className="chat-bubble w-[300px] px-3">
+          <div >
+            <div className="chat chat-start " >
+              <div className="chat-bubble w-[300px] px-3 dark:bg-gray-700 bg-gray-50 dark:shadow-none dark:border-transparent  shadow-lg shadow-gray-300/30 border border-gray-100">
                 <div
-                  className={`  mx-0 py-2 px-2 mb-2 w-full rounded-lg flex gap-2.5 cursor-pointer transition-all duration-200 overflow-hidden bg-indigo-400/30 hover:bg-indigo-400/60 `}
+                  data-rp-color={color}
+                  className={`  mx-0 py-2 px-2 mb-2 w-full rounded-lg flex gap-2.5 cursor-pointer transition-all duration-200 overflow-hidden  `}
                 >
-                  <p className="w-[2px] bg-gray-300 rounded-full "></p>
+                  <p
+                  data-rp-border-color={color}
+                    className="w-[2.5px]  rounded-full "
+                  ></p>
 
                   <div className="flex flex-col  ">
-                    <p
-                      className={`font-semibold  text-[14px]  text-indigo-500`}
-                      dir="auto"
-                    >
+                    <p data-name-color={color} className={`font-semibold  text-[14px]`} dir="auto">
                       Abolfazl
                     </p>
-                    <p className="text-[13px] truncate text-gray-300">
+                    <p className="text-[13px] truncate dark:text-gray-300 text-gray-600">
                       Replay to your message
                     </p>
                   </div>
                 </div>
-                <p className='text-gray-100 text-sm  leading-5'>Your name and replies to your message will be shown in the selected color</p>
+                <p className="dark:text-gray-100 text-sm  leading-5 text-gray-700">
+                  Your name and replies to your message will be shown in the
+                  selected color
+                </p>
               </div>
             </div>
           </div>
 
-          <div className='grid grid-cols-6 my-5 place-items-center gap-3'>
-              
-              <div className=' border-2 border-transparent w-10 h-10 grid place-items-center rounded-full'>
-                <span className='w-8 h-8 rounded-full bg-[#5dadde] inline-block cursor-pointer'></span>
-              </div>
-              <div className=' border-2 border-transparent w-10 h-10 grid place-items-center rounded-full'>
-                <span className='w-8 h-8 rounded-full bg-[#79ca7b] inline-block cursor-pointer'></span>
-              </div>
-              <div className=' border-2 border-transparent w-10 h-10 grid place-items-center rounded-full'>
-                <span className='w-8 h-8 rounded-full bg-[#e39652] inline-block cursor-pointer'></span>
-              </div>
-              <div className=' border-2 border-transparent w-10 h-10 grid place-items-center rounded-full'>
-                <span className='w-8 h-8 rounded-full bg-[#e35a62] inline-block cursor-pointer'></span>
-              </div>
-              <div className=' border-2 border-transparent w-10 h-10 grid place-items-center rounded-full'>
-                <span className='w-8 h-8 rounded-full bg-[#a380dc] inline-block cursor-pointer'></span>
-              </div>
-            
-              <div className=' border-2 border-transparent w-10 h-10 grid place-items-center rounded-full'>
-                <span className='w-8 h-8 rounded-full bg-[#e85089] inline-block cursor-pointer'></span>
-              </div>
-            
-              
+          <div className="grid grid-cols-6 my-5 place-items-center gap-3">
+            <div
+              className={` border-2  w-10 h-10 grid place-items-center rounded-full transition-all duration-200 ${
+                color === 'blue' ? 'border-[#5dadde]' : 'border-transparent'
+              }`}
+            >
+              <span
+                className="w-8 h-8 rounded-full bg-[#5dadde] inline-block cursor-pointer"
+                onClick={() => {setColor('blue')
+                  setIsChange(true)
+                }}
+              ></span>
+            </div>
+            <div
+              className={` border-2  w-10 h-10 grid place-items-center rounded-full transition-all duration-200 ${
+                color === 'green' ? 'border-[#79ca7b]' : 'border-transparent'
+              }`}
+            >
+              <span
+                className="w-8 h-8 rounded-full bg-[#79ca7b] inline-block cursor-pointer"
+                onClick={() => {setColor('green')
+                  setIsChange(true)
+                }}
+              ></span>
+            </div>
+            <div
+              className={` border-2  w-10 h-10 grid place-items-center rounded-full transition-all duration-200 ${
+                color === 'orange' ? 'border-[#e39652]' : 'border-transparent'
+              }`}
+            >
+              <span
+                className="w-8 h-8 rounded-full bg-[#e39652] inline-block cursor-pointer"
+                onClick={() => {setColor('orange')
+                  setIsChange(true)
+                }}
+              ></span>
+            </div>
+            <div
+              className={` border-2  w-10 h-10 grid place-items-center rounded-full transition-all duration-200 ${
+                color === 'red' ? 'border-[#e35a62]' : 'border-transparent'
+              }`}
+            >
+              <span
+                className="w-8 h-8 rounded-full bg-[#e35a62] inline-block cursor-pointer"
+                onClick={() => {setColor('red')
+                  setIsChange(true)
+                }}
+              ></span>
+            </div>
+            <div
+              className={` border-2  w-10 h-10 grid place-items-center rounded-full transition-all duration-200 ${
+                color === 'purple' ? 'border-[#a380dc]' : 'border-transparent'
+              }`}
+            >
+              <span
+                className="w-8 h-8 rounded-full bg-[#a380dc] inline-block cursor-pointer"
+                onClick={() => {setColor('purple')
+                  setIsChange(true)
+                }}
+              ></span>
+            </div>
+
+            <div
+              className={` border-2  w-10 h-10 grid place-items-center rounded-full transition-all duration-200 ${
+                color === 'rose' ? 'border-[#e85089]' : 'border-transparent'
+              }`}
+            >
+              <span
+                className="w-8 h-8 rounded-full bg-[#e85089] inline-block cursor-pointer"
+                onClick={() => {setColor('rose')
+                  setIsChange(true)
+                }}
+              ></span>
+            </div>
           </div>
         </div>
         <p className="border-[7px] border-base-200 w-[150%] -mx-[30px]"></p>
