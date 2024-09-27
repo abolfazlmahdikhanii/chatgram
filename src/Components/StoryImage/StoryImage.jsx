b4b093160070b9e31ba9da57267e75b1b5436a40
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-27 16:21:49 +0330

    modified

--- src/Components/StoryImage/StoryImage.jsx
+++ src/Components/StoryImage/StoryImage.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { supabase } from '../../superbase'
 
-const StoryImage = ({ src }) => {
+const StoryImage = ({ src,onLoad }) => {
   const [storyLoading, setStoryLoading] = useState(false)
+  const [url,setUrl]=useState("")
+
+  useEffect(()=>{
+    if(src) download(src)
+  },[])
+
+  const download = useCallback(async (path) => {
+    const groupFile = []
+
+    // if (findItem(messageId)) return false
+    supabase.storage
+      .from('uploads')
+      .download(path)
+      .then((res) => {
+        const fr = new FileReader()
+        fr.addEventListener('load', (ev) => {
+          setUrl(ev.target.result)
+        })
+        fr.readAsDataURL(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
   return (
     <>
       <img
-        src={src}
+        src={url}
         onLoad={() => setStoryLoading(true)}
         alt=""
         className={`w-full h-auto object-cover aspect-video`}
+       onError={(e)=>e.currentTarget.innerHTML=`<div className="bg-black"></div>`}
       />
       {!storyLoading && (
         <span className="loading loading-spinner loading-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></span>
