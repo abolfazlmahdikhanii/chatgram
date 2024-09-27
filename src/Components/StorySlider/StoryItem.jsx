018bde362cd7b41a31ec802515e629780ed2426c
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-09-27 16:18:43 +0330

    modified

--- src/Components/StorySlider/StoryItem.jsx
+++ src/Components/StorySlider/StoryItem.jsx
@@ -1,59 +1,112 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaPlus } from 'react-icons/fa6'
+import userNameSpliter from '../../Utility/userNameSpliter'
+import { supabase } from '../../superbase'
 
 const StoryItem = ({
-  userName,
-  profileImg,
   onStoryClick,
   addStory,
   onCreateStory,
-  relation,
+  chats,
+  saveChat,
 }) => {
+  let profile = null
+  if (chats) profile = chats
+  if (saveChat) profile = saveChat
+  const [isStory,setIsStory]=useState(false)
+  useEffect(()=>{
+    checkUserHasStory(profile?.userid)
+    
+      // // Subscribe to real-time changes in the chat_requests table
+      const subscription = supabase
+        .channel('public:stories')
+        .on(
+          'postgres_changes',
+          { event: '*', schema: 'public', table: 'stories' },
+          (payload) => {
+            // Handle insert, update, delete events
+         
+            checkUserHasStory(profile?.userid)
+          }
+        )
+        .subscribe()
+
+  },[])
+  const checkUserHasStory = async (id) => {
+    try {
+      let { data: stories, error } = await supabase
+        .from('stories')
+        .select('*')
+        .eq('userid', id)
+        
+      if (error) throw Error
+   
+        if(stories&&stories?.length>0) setIsStory(true)
+    
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+  console.log(isStory);
+
   return (
     <div
-      className="flex flex-col gap-2 justify-center items-center "
-      onClick={onStoryClick}
+      className={` flex-col gap-2 justify-center items-center ${!isStory&&!saveChat?'hidden':'flex'}`}
+      onClick={()=>{isStory?onStoryClick(profile?.userid):null}}
     >
       <div className="p-1 relative ">
-        <div className="w-[70px] h-[70px] mask mask-squircle z-[5] relative">
-          <img src={profileImg} alt="story img" />
+        <div
+          data-color={!saveChat ? profile?.bgProfile : 'purple'}
+          className="w-[65px] h-[65px] mask mask-squircle z-[5] relative grid place-items-center"
+        >
+          {profile?.avatar_url ? (
+            <img src={profile?.avatar_url} alt="story img" />
+          ) : (
+            <span className="text-2xl text-white font-bold">
+              {userNameSpliter(
+                profile?.username || profile?.email?.split('@')[0]
+              )}
+            </span>
+          )}
         </div>
 
         {/* border */}
 
         <div className="w-full h-full absolute top-0 left-0  mask mask-squircle bg-base-100 z-[3]"></div>
-        {!addStory ? (
-          <>
-            <div
-              data-color="purple"
-              className="w-[85px] h-[86px]  absolute -top-1 -left-1  mask mask-squircle  z-[2] "
-            ></div>
-            {relation == 'me' ? (
-              <div className="absolute -bottom-1 right-0 z-40">
-                <button
-                  data-color="violet"
-                  className="  mask mask-squircle w-7 h-7 text-white grid place-items-center"
-                  onClick={onCreateStory}
-                >
-                  +
-                </button>
-              </div>
-            ) : null}
-          </>
-        ) : (
-          <div className="absolute -bottom-1 right-0 z-20">
+        {isStory ? (
+          <div
+            data-color="purple"
+            className="w-[80px] h-[81px]  absolute -top-1 -left-1  mask mask-squircle  z-[2] "
+          ></div>
+        ) : null}
+        {saveChat ? (
+          <div className="absolute -bottom-1 -right-2 z-40">
             <button
-              data-color="violet"
-              className="  mask mask-squircle w-7 h-7 text-white grid place-items-center"
+              data-color="yellow"
+              className="  mask mask-squircle w-6 h-6 text-white grid place-items-center"
               onClick={onCreateStory}
             >
-              +
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
             </button>
           </div>
-        )}
+        ) : null}
       </div>
-      <p className="dark:text-gray-200 font-medium truncate text-gray-800">
-        {userName}
+      <p className="dark:text-gray-200 font-medium truncate text-gray-800 max-w-[90px] text-center">
+        {!saveChat ? profile?.username || profile?.email?.split('@')[0] : 'You'}
       </p>
     </div>
   )
