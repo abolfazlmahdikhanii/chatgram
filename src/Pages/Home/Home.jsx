d0cf18b772e1a7f9dc40ced158479f6cae29a779
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-10-28 21:02:42 +0330

    modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="w-8/12 flex flex-col items-center justify-center mx-auto h-screen gap-16 p-4 ">
+    <div className="w-8/12  flex-col items-center justify-center mx-auto h-screen gap-16 p-4 flex ">
     <div>
         <img src="search-chat.svg" alt="" />
     </div>
