a34c71a7adeba94494e7b386e816f031b405b470
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-10-29 21:00:50 +0330

    add option for toast

---
+++ src/Utility/toastOption.js
@@ -0,0 +1,15 @@
+import { toast } from "react-toastify";
+
+export const toastOptions = {
+    position: toast.POSITION.BOTTOM_CENTER,
+    autoClose: 1200,
+    hideProgressBar: true,
+    closeOnClick: false,
+    pauseOnHover: false,
+    draggable: false,
+    progress: undefined,
+    theme: 'dark',
+    closeButton: false,
+    className:
+      'px-5 py-2.5 bg-slate-500/20 rounded-xl text-white backdrop-blur',
+  }
