c7d754f0f704e1280dbe8501ad6ee6155a9edd16
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-10-31 22:16:52 +0330

    modified

--- src/Components/FIleItem/FileItem.jsx
+++ src/Components/FIleItem/FileItem.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { MdOutlineDeleteOutline } from "react-icons/md";
-import FileIcon from "../UI/FIleIcon/FileIcon";
-import ProgressFile from "../UI/ProgressFile/ProgressFile";
+import React, { useEffect } from 'react'
+import { MdOutlineDeleteOutline } from 'react-icons/md'
+import FileIcon from '../UI/FIleIcon/FileIcon'
+import ProgressFile from '../UI/ProgressFile/ProgressFile'
+import { useState } from 'react'
 
 const FileItem = ({
   type,
@@ -13,117 +14,133 @@ const FileItem = ({
   from,
   onContext,
   isColor,
-  isFile
+  isFile,
+  progress,
+  isCompletedUploaded,
+  onDownload,
 }) => {
+
+
   const formatSize = (bytes) => {
-    if (bytes == 0) return "0 Bytes";
+    if (bytes == 0) return '0 Bytes'
     var k = 1024,
-      sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
-      i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed()) + " " + sizes[i];
-  };
+      sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+      i = Math.floor(Math.log(bytes) / Math.log(k))
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i]
+  }
   return (
     <>
-     {
-      !message?(
+      {!message ? (
         <DefaultFile
-         type={type}
-         src={src}
-         message={message}
-         from={from}
-         size={size}
-         name={name}
-         onRemove={onRemove}
-         formatSize={formatSize}
-         onContext={onContext}
+          type={type}
+          src={src}
+          message={message}
+          from={from}
+          size={size}
+          name={name}
+          onRemove={onRemove}
+          formatSize={formatSize}
+          onContext={onContext}
+          progress={progress}
       
-         isColor={isColor}
         />
-      ):
-      (
+      ) : (
         <MessageFile
-         type={type}
-         src={src}
-         message={message}
-         from={from}
-         size={size}
-         name={name}
-         formatSize={formatSize}
-         onRemove={onRemove}
-         onContext={onContext}
-isColor={isColor}
-   
+          type={type}
+          src={src}
+          message={message}
+          from={from}
+          size={size}
+          name={name}
+          formatSize={formatSize}
+          onRemove={onRemove}
+          onContext={onContext}
+          isColor={isColor}
+          progress={progress}
+          isCompletedUploaded={isCompletedUploaded}
+          onDownload={onDownload}
         />
-      )
-     }
+      )}
     </>
-  );
-};
-const DefaultFile=({type,src,message,from,size,name,onRemove,formatSize,onContext,isColor})=>{
-  return(
- 
-      <li
-    className={`file-item relative  w-full px-3`}
-    onContextMenu={onContext}
-  >
-    <div>
-      <FileIcon
-        type={type}
-        path={src ? src : ""}
-        message={message}
-        from={from}
-        isColor={isColor}
-      />
-    </div>
-    <div className="flex flex-col gap-2 max-w-[200px]">
-      <p
-        className={`text-lg text-white font-semibold truncate w-full `}
-      >
-        {name}
-      </p>
-      <p >{formatSize(size)}</p>
-     
-    </div>
-  
+  )
+}
+const DefaultFile = ({
+  type,
+  src,
+  message,
+  from,
+  size,
+  name,
+  onRemove,
+  formatSize,
+  onContext,
+  isColor,
+
+}) => {
+  return (
+    <li className={`file-item relative  w-full px-3`} onContextMenu={onContext}>
+      <div>
+        <FileIcon
+          type={type}
+          path={src ? src : ''}
+          message={message}
+          from={from}
+          isColor={isColor}
+        />
+      </div>
+      <div className="flex flex-col gap-2 max-w-[200px]">
+        <p className={`text-lg dark:text-white font-semibold truncate w-full text-gray-700`}>
+          {name}
+        </p>
+        <p className='text-gray-500 dark:text-gray-300'>{formatSize(size)}</p>
+      </div>
+
       <div className="self-center  w-full flex justify-end">
         <button className="btn btn-square btn-sm " onClick={onRemove}>
           <MdOutlineDeleteOutline size={21} />
         </button>
       </div>
-    
-  </li>
-
-
+    </li>
   )
 }
-const MessageFile=({type,src,message,from,size,name,formatSize,onRemove,onContext,isColor})=>{
-  return(
-
-    <li
-  className={`file-item relative  w-full hover:bg-transparent`}
-  onContextMenu={onContext}
->
-  <div>
-    <FileIcon
-      type={type}
-      path={src ? src : ""}
-      message={message}
-      from={from}
-      isColor={isColor}
-    />
-  </div>
-  <div className="flex flex-col gap-2 max-w-[200px]">
-    <p
-      className={`text-sm text-white font-semibold truncate w-full `}
-    >
-      {name}
-    </p>
-    <p className={`text-xs`}>{formatSize(size)}</p>
-    <ProgressFile onRemove={onRemove} />
-  </div>
+const MessageFile = ({
+  type,
+  src,
+  message,
+  from,
+  size,
+  name,
+  formatSize,
+  onRemove,
+  onContext,
+  isColor,
+  progress,
+isCompletedUploaded,
+onDownload
+}) => {
  
-</li>
-
+  return (
+    <li
+      className={`file-item-1 relative  w-full hover:bg-transparent`}
+      onContextMenu={onContext}
+    >
+      <div>
+        <FileIcon
+          type={type}
+          path={src ? src : ''}
+          message={message}
+          from={from}
+          isColor={isColor}
+        />
+      </div>
+      <div className="flex flex-col gap-2 max-w-[200px]">
+        <p className={`text-sm dark:text-white font-semibold truncate w-full text-gray-600 `}>
+          {name}
+        </p>
+        <p className={`text-[12px] dark:text-gray-300 text-gray-500`}>{formatSize(size)}</p>
+        <ProgressFile progress={progress} isCompletedUploaded={isCompletedUploaded} onDownload={onDownload}  />
+      </div>
+    </li>
   )
 }
-export default FileItem;
+export default FileItem
