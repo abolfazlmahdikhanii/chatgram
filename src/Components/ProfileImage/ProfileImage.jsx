370e2ce3324aaf3078cf3d0a01fbbf8577ab0772
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-10-31 22:19:09 +0330

    modified

--- src/Components/ProfileImage/ProfileImage.jsx
+++ src/Components/ProfileImage/ProfileImage.jsx
@@ -17,25 +17,29 @@ const ProfileImage = ({
   return (
     <div className="avatar">
       <div
-        className={`mask mask-squircle ${size!=='m'&&size!=='xs'&&size!=='lg'?'w-11 h-11':''} ${
-          size === 'm' && 'w-14 h-14 text-lg'
-        } ${size === 'xs' && 'w-5 h-5 text-[9px]'} ${size === 'lg' && 'w-32 h-32 text-4xl'}`}
+        className={`mask mask-squircle ${
+          size !== 'm' && size !== 'xs' && size !== 'lg' ? 'w-11 h-11' : ''
+        } ${size === 'm' && 'w-14 h-14 text-lg'} ${
+          size === 'xs' && 'w-5 h-5 text-[9px]'
+        } ${size === 'lg' && 'w-32 h-32 text-4xl'}`}
       >
         {src && !isSave ? (
-          <img src={src} alt="profile" className="w-full h-full aspect-square  " />
+          <img
+            src={src}
+            alt="profile"
+            className="w-full h-full aspect-square"
+            onError={e=>e.currentTarget.src="./avatar.png"}
+          
+          />
         ) : (
           <div
-            data-color={!isSave?bgProfile:'purple'}
+            data-color={!isSave ? bgProfile : 'purple'}
             className={`grid place-items-center text-white h-full font-bold w-full  `}
           >
             {!src && !isSave && (
-              <span>
-                {userName
-                  && userNameSpliter(userName)
-                }
-              </span>
+              <span>{userName && userNameSpliter(userName)}</span>
             )}
-            { isSave && (
+            {isSave && (
               <span>
                 <BsFillBookmarkFill size={sizes} color="#fff" />
               </span>
