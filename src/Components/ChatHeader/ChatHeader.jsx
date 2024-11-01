731319e6f3c2d9283634c1a7731aaedf27e19964
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2024-11-01 12:58:41 +0330

    modified

--- src/Components/ChatHeader/ChatHeader.jsx
+++ src/Components/ChatHeader/ChatHeader.jsx
@@ -34,7 +34,7 @@ const ChatHeader = ({
     chatId,
     clearHistory,
     checkMessage,
-    setFriendID
+    setFriendID,
   } = useContext(ChatContext)
 
   return (
@@ -82,10 +82,10 @@ const HeaderMessage = ({
   checkMessage,
   chatId,
   isMessage,
-  setFriendID
+  setFriendID,
 }) => {
   const { user } = useContext(UserContext)
-   const navigate=useNavigate()
+  const navigate = useNavigate()
   return (
     <section className="md:px-5 flex items-center justify-between relative">
       {info ? (
@@ -93,8 +93,8 @@ const HeaderMessage = ({
           <button
             className="p-2.5 rounded-full btn-ghost  grid place-items-center transition-all duration-200"
             onClick={() => {
-              navigate("/")
-              setFriendID("")
+              navigate('/')
+              setFriendID('')
             }}
           >
             <AiOutlineArrowLeft size={20} />
@@ -262,27 +262,29 @@ const HeaderMessage = ({
 
           <p className={`font-[700] text-[14px] ml-1`}>Clear History</p>
         </div>
-        <div
-          className=" select-box--item text-red-500 hover:bg-red-400/20"
-          onClick={chatId!==user?.userid?DeleteChat:null}
-        >
-          <svg
-            width={18}
-            height={18}
-            viewBox="0 0 19 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-red-500"
+        {chatId !== user?.userid ? (
+          <div
+            className=" select-box--item text-red-500 hover:bg-red-400/20"
+            onClick={DeleteChat}
           >
-            <path
-              d="M16.459 7.012c.199 0 .38.087.523.234.133.157.2.352.18.558 0 .068-.532 6.808-.837 9.645-.19 1.741-1.313 2.798-2.996 2.827-1.295.029-2.56.039-3.806.039-1.322 0-2.616-.01-3.871-.04-1.627-.038-2.75-1.114-2.932-2.826-.313-2.847-.836-9.577-.846-9.645a.79.79 0 01.19-.558.706.706 0 01.524-.234h13.87zM11.584.315c.884 0 1.674.617 1.903 1.497l.163.73a1.28 1.28 0 001.24 1.016h2.917c.389 0 .713.323.713.734v.38a.73.73 0 01-.713.734H1.233a.73.73 0 01-.713-.734v-.38c0-.411.323-.734.713-.734H4.15c.592 0 1.108-.421 1.241-1.015l.153-.682C5.78.93 6.56.315 7.455.315h4.13z"
-              stroke="currentColor"
-              strokeWidth={1.5}
-            />
-          </svg>
+            <svg
+              width={18}
+              height={18}
+              viewBox="0 0 19 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-red-500"
+            >
+              <path
+                d="M16.459 7.012c.199 0 .38.087.523.234.133.157.2.352.18.558 0 .068-.532 6.808-.837 9.645-.19 1.741-1.313 2.798-2.996 2.827-1.295.029-2.56.039-3.806.039-1.322 0-2.616-.01-3.871-.04-1.627-.038-2.75-1.114-2.932-2.826-.313-2.847-.836-9.577-.846-9.645a.79.79 0 01.19-.558.706.706 0 01.524-.234h13.87zM11.584.315c.884 0 1.674.617 1.903 1.497l.163.73a1.28 1.28 0 001.24 1.016h2.917c.389 0 .713.323.713.734v.38a.73.73 0 01-.713.734H1.233a.73.73 0 01-.713-.734v-.38c0-.411.323-.734.713-.734H4.15c.592 0 1.108-.421 1.241-1.015l.153-.682C5.78.93 6.56.315 7.455.315h4.13z"
+                stroke="currentColor"
+                strokeWidth={1.5}
+              />
+            </svg>
 
-          <p className={`font-[700] text-[14px] ml-1 text-red-500`}>Delete</p>
-        </div>
+            <p className={`font-[700] text-[14px] ml-1 text-red-500`}>Delete</p>
+          </div>
+        ) : null}
       </div>
     </section>
   )
